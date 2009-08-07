LINBIT/drbd

commit a3bd752eed8c98dca48f4decd103871d9787df3c

drbdadm: don't try to set res->peer too early; rename --to to --peer
set_peer_in_resource may set config_valid=0 and exit.  bailing out from
post_parse() because of unrelated (to the current invocation) resources
is bad.

try to set_peer_in_resource as late as possible.

also rename --to to --peer
(it got renamed from --host to --to earlier,
but that renaming was incomplete anyways)

user/drbdadm_main.c
@@ -331,6 +331,9 @@ int call_cmd_fn(int (*function) (struct d_resource *, const char *),
 int call_cmd(struct adm_cmd *cmd, struct d_resource *res,
 	     enum on_error on_error)
 {
+	if (!res->peer && cmd->need_peer)
+		set_peer_in_resource(res, PEER_REQUIRED);
+
 	return call_cmd_fn(cmd->function, cmd->name, res, on_error);
 }
 
@@ -367,7 +370,7 @@ struct option admopt[] = {
 	{"drbd-proxy-ctl", required_argument, 0, 'p'},
 	{"sh-varname", required_argument, 0, 'n'},
 	{"force", no_argument, 0, 'f'},
-	{"to", required_argument, 0, 't'},
+	{"peer", required_argument, 0, 'P'},
 	{0, 0, 0, 0}
 };
 
@@ -1668,9 +1671,11 @@ int adm_connect(struct d_resource *res,
 		make_address(res->me->proxy->inside_addr,
 			     res->me->proxy->inside_port,
 			     res->me->proxy->inside_af);
-	} else {
+	} else if (res->peer) {
 		make_address(res->peer->address, res->peer->port,
 			     res->peer->address_family);
+	} else {
+		argv[NA(argc)] = "N/A";
 	}
 	argv[NA(argc)] = res->protocol;
 
@@ -1756,6 +1761,12 @@ static int do_proxy(struct d_resource *res, int do_up)
 		fprintf(stderr,
 			"The proxy config in resource %s is not for %s.\n",
 			res->name, nodeinfo.nodename);
+		exit(E_config_invalid);
+	}
+
+	if (!res->peer) {
+		fprintf(stderr, "Cannot determine the peer in resource %s.\n",
+			res->name);
 		exit(E_config_invalid);
 	}
 
@@ -2828,7 +2839,7 @@ int parse_options(int argc, char **argv)
 		case 'f':
 			force = 1;
 			break;
-		case 'h':
+		case 'P':
 			connect_to_host = optarg;
 			break;
 		case '?':
@@ -3111,7 +3122,7 @@ int main(int argc, char **argv)
 	if (!config_valid)
 		exit(E_config_invalid);
 
-	post_parse(config, cmd->need_peer);
+	post_parse(config);
 
 	if (!is_dump || dry_run || verbose)
 		expand_common();

user/drbdadm.h
@@ -175,7 +175,7 @@ enum pr_flags {
   IgnDiscardMyData = 8
 };
 extern struct d_resource* parse_resource(char*, enum pr_flags);
-extern void post_parse(struct d_resource *config, int need_peer);
+extern void post_parse(struct d_resource *config);
 extern struct d_option *new_opt(char *name, char *value);
 extern int name_in_names(char *name, struct d_name *names);
 extern char *_names_to_str(char* buffer, struct d_name *names);
@@ -185,7 +185,8 @@ extern char *_names_to_str_c(char* buffer, struct d_name *names, char c);
 #define names_to_str_c(N, C) _names_to_str_c(alloca(NAMES_STR_SIZE+1), N, C)
 extern void free_names(struct d_name *names);
 extern void set_me_in_resource(struct d_resource* res);
-extern void set_peer_in_resource(struct d_resource* res, int required);
+enum set_peer_flags { PEER_REQUIRED = 1, FROM_ADJUST = 2 };
+extern void set_peer_in_resource(struct d_resource* res, enum set_peer_flags flags);
 extern void set_on_hosts_in_res(struct d_resource *res);
 extern void set_disk_in_res(struct d_resource *res);
 

user/drbdadm_adjust.c
@@ -211,7 +211,8 @@ int adm_adjust(struct d_resource* res,char* unused __attribute((unused)))
 	running = parse_resource(config_file_dummy, IgnDiscardMyData);
 	fclose(yyin);
 	waitpid(pid,0,0);
-	post_parse(running, 0);
+	post_parse(running);
+	set_peer_in_resource(running, FROM_ADJUST);
 
 	do_attach  = !opts_equal(res->disk_options, running->disk_options);
 	if(running->me) {

user/drbdadm_parser.c
@@ -1107,7 +1107,7 @@ void set_me_in_resource(struct d_resource* res)
 	}
 }
 
-void set_peer_in_resource(struct d_resource* res, int required)
+void set_peer_in_resource(struct d_resource* res, enum set_peer_flags flags)
 {
 	struct d_host_info *host;
 
@@ -1118,24 +1118,24 @@ void set_peer_in_resource(struct d_resource* res, int required)
 
 	/* Determin the peer host section */
 	for (host = res->all_hosts; host; host=host->next) {
-		if (host == res->me)
-			continue;
+		if (!connect_to_host || (flags & FROM_ADJUST)) {
+			if (host == res->me)
+				continue;
 
-		if (!connect_to_host) {
-			/* No, --host option given, assume there are only two on sections. */
+			/* connect_to_host is NULL, no --peer option given.
+			 * There should only be two "on" sections. */
 			if (!res->peer)
 				res->peer = host;
 			else {
 				res->peer = NULL;
-				if (!required)
+				if (!(flags & PEER_REQUIRED))
 					return;
 
 				config_valid = 0;
 				fprintf(stderr,
 					"%s:%d: in resource %s, %s %s { ... } ... %s %s { ... }:\n"
 					"\tThere are multiple host sections for the peer node.\n"
-					"\tYou need to use the --host option to select one host\n"
-					"\tsection as the peer host section.\n",
+					"\tUse the --peer option to select the peer section.\n",
 					res->config_file, host->config_line, res->name,
 					res->me->lower ? "stacked-on-top-of" : "on",
 					res->me->lower ? res->me->lower->name : names_to_str(res->me->on_hosts),
@@ -1147,6 +1147,14 @@ void set_peer_in_resource(struct d_resource* res, int required)
 			if (name_in_names(connect_to_host, host->on_hosts) ||
 			    (host->by_address && !strcmp(connect_to_host, host->address)) ||
 			    ( host->proxy && name_in_names(nodeinfo.nodename, host->proxy->on_hosts))) {
+				if (host == res->me) {
+					fprintf(stderr,
+						"%s:%d: in resource %s\n"
+						"\tInvoked with --peer '%s', but that matches myself!\n",
+						res->config_file, host->config_line, res->name, connect_to_host);
+					config_valid = 0;
+					return;
+				}
 				if (res->peer) {
 					/* Should have been catched by check_unique ... */
 					config_valid = 0;
@@ -1158,13 +1166,12 @@ void set_peer_in_resource(struct d_resource* res, int required)
 		}
 	}
 
-	if (required && !res->peer) {
+	if ((flags & PEER_REQUIRED) && !res->peer) {
 		config_valid = 0;
 		if (connect_to_host)
 			fprintf(stderr,
 				"%s:%d: in resource %s:\n"
-				"\tNo host ('on') section matches the host you passed\n"
-				"\twith the --host ('%s')option.\n",
+				"\tNo host ('on') section matches the requested --peer '%s'\n",
 				res->config_file, res->start_line, res->name, connect_to_host);
 		else
 			fprintf(stderr,
@@ -1393,7 +1400,7 @@ struct d_resource* parse_resource(char* res_name, enum pr_flags flags)
 	return res;
 }
 
-void post_parse(struct d_resource *config, int need_peer)
+void post_parse(struct d_resource *config)
 {
 	struct d_resource *res,*tmp;
 
@@ -1403,9 +1410,6 @@ void post_parse(struct d_resource *config, int need_peer)
 
 	for_each_resource(res, tmp, config)
 		set_me_in_resource(res); // Needs "on_hosts" set in this res
-
-	for_each_resource(res, tmp, config)
-		set_peer_in_resource(res, need_peer); // Needs me already set
 
 	for_each_resource(res, tmp, config)
 		if (res->stacked_on_one)

LINBIT/drbd-utils

commit 63494d79cb950b622b931bfd1a74c41df104958e

Fixed regression introduced with a3bd752eed8c98dca48f4decd103871d9787df3c (BugZ 241)
  Always try to set the peer. Only fail if that command needs the peer set.

user/drbdadm_main.c
@@ -333,8 +333,8 @@ int call_cmd_fn(int (*function) (struct d_resource *, const char *),
 int call_cmd(struct adm_cmd *cmd, struct d_resource *res,
 	     enum on_error on_error)
 {
-	if (!res->peer && cmd->need_peer)
-		set_peer_in_resource(res, 1);
+	if (!res->peer)
+		set_peer_in_resource(res, cmd->need_peer);
 
 	return call_cmd_fn(cmd->function, cmd->name, res, on_error);
 }