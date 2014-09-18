7c47bfcaa5fcd46b4db48f71331b9a81efabb943
Lars Ellenberg <lars.ellenberg@linbit.com>
2014-09-18 15:00:02 +0200

    stonith_admin-fence-peer.sh: check for minors to be UpToDate
    Just as with rhcs_fence or crm-fence-peer, this must check
    that all minors are UpToDate first.
    
    Also add --tolerance 5s to avoid strange effects
    if there is a lot of shooting going on.
    
    pseudo code:
    
    	check that we have victims
    	check that all minors are up to date
    	for all victims,
    		try to kill it directly via stonith_admin, and be happy
    		or be unhappy
    
    	if still happy, exit 7 ("Yes, I've managed to fence all victims")

--- scripts/stonith_admin-fence-peer.sh
+++ scripts/stonith_admin-fence-peer.sh
@@ -24,19 +24,42 @@ log() {
   logger -i -t "`basename $0`" -s "$msg"
 }
 
-if [ -z "$DRBD_PEERS" ]; then
-  log "DRBD_PEERS is empty or unset, cannot continue."
-  exit 1
-fi
+die() { log "$*"; exit 1; }
+
+die_unless_all_minors_up_to_date()
+{
+	set -- $DRBD_MINOR
+	local n_minors=$#
+
+	[ $n_minors != 0 ] ||
+		die "Resource minor numbers unknown! Unable to proceed."
+
+	# and build a "grep extended regex"
+	local _OLDIFS=$IFS
+	IFS="|"
+	local minor_regex="^ *($*): cs:"
+	IFS=$_OLDIFS
+
+	# grep -c -Ee '^ *(m|i|n|o|r|s): cs:.* ds:UpToDate' /proc/drbd
+	local proc_drbd=$(cat /proc/drbd)
+	local minors_of_resource=$(echo "$proc_drbd" | grep -E -e "$minor_regex")
+	local n_up_to_date=$(echo "$minors_of_resource" | grep -c -e "ds:UpToDate")
+
+	log "n_minors: $n_minors; n_up_to_date: $n_up_to_date"
+	[ "$n_up_to_date" = "$n_minors" ] ||
+		die "$DRBD_RESOURCE(minor $DRBD_MINOR): some minor is not UpToDate, will not fence peer."
+}
+
+[ -n "$DRBD_PEERS" ] || die "DRBD_PEERS is empty or unset, cannot continue."
+die_unless_all_minors_up_to_date
 
 for p in $DRBD_PEERS; do
-  stonith_admin --tag drbd --fence $p
+  stonith_admin --tolerance 5s --tag drbd --fence $p
   rc=$?
   if [ $rc -eq 0 ]; then
     log "stonith_admin successfully fenced peer $p."
   else
-    log "Failed to fence peer $p. stonith_admin returned $rc."
-    exit 1
+    die "Failed to fence peer $p. stonith_admin returned $rc."
   fi
 done
 
