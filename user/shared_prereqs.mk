9f63e920f1aed47b39c8ba96fcd5ae7a816dad3d
Lars Ellenberg <lars.ellenberg@linbit.com>
2016-04-14 12:27:21 +0200

    build: fix auto dependency generation (again)

--- user/shared_prereqs.mk
+++ user/shared_prereqs.mk
@@ -12,8 +12,8 @@ drbd_buildtag.o: ../shared/drbd_buildtag.c
 	rm -f $@.$$$$
 
 .drbdmeta_scanner.d: ../shared/drbdmeta_scanner.c
-all-dep = $($(filter-out drbd_buildtag.o,$(all-obj)):%.o=.%.d)
+all-dep := $(filter-out drbd_buildtag.d,$(all-obj:%.o=.%.d))
 
-ifneq (,$(filter-out clean distclean,$(MAKECMDGOALS)))
-include $(all-dep)
+ifeq ($(MAKECMDGOALS),$(filter-out clean distclean,$(MAKECMDGOALS)))
+-include $(all-dep)
 endif
