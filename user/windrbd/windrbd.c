f0b150b6977944836ec1815235107394d92b3bfe
Johannes Thoma <johannes@johannesthoma.com>
2018-01-24 19:13:18 +0100

    Working on tstrings.
    swprintf does not support computing the length without a buffer.
    Argggh.

--- user/windrbd/windrbd.c
+++ user/windrbd/windrbd.c
@@ -1,18 +1,58 @@
+#define UNICODE 1
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int assign_drive_letter(int minor, const char *drive)
-{
-	printf("Assigning drive letter %s to minor %d\n", drive, minor);
-
-	return 0;
-}
+#include <windows.h>
+#include <wchar.h>
 
 void usage_and_exit(void)
 {
 	fprintf(stderr, "Usage: windrbd assign-drive-letter <minor> <drive-letter>\n");
 	exit(1);
+}
+
+int assign_drive_letter(int minor, const char *drive)
+{
+	LPTSTR t_device, t_drive;
+	int device_len, drive_len;
+	BOOL ret;
+
+	if (!isalpha(drive[0])) {
+		fprintf(stderr, "Drive letter (%s) must start with a letter.\n", drive);
+		usage_and_exit();
+	}
+	if (drive[1] != '\0') {
+		if (drive[1] != ':' || drive[2] != '\0') {
+			fprintf(stderr, "Drive letter (%s) must end with a colon\n", drive);
+			usage_and_exit();
+		}
+	}
+	device_len = swprintf(NULL, 0, L"\\Device\\Drbd%d", minor)+1;
+	drive_len = swprintf(NULL, 0, L"%s", drive)+1;
+
+	t_device = malloc(device_len * sizeof(*t_device));
+	if (t_device == NULL) {
+		fprintf(stderr, "Sorry out of memory.\n");
+	}
+	t_drive = malloc(drive_len * sizeof(*t_drive));
+	if (t_drive == NULL) {
+		fprintf(stderr, "Sorry out of memory.\n");
+	}
+
+	swprintf(t_device, device_len, L"\\Device\\Drbd%d", minor);
+	swprintf(t_drive, drive_len, L"%s", drive);
+
+	wprintf(L"Assigning drive letter %ls to minor %d (device %ls)\n", t_drive, minor, t_device);
+
+	ret = DefineDosDevice(DDD_RAW_TARGET_PATH, t_device, t_drive);
+	if (!ret) {
+		int err;
+		err = GetLastError();
+		fprintf(stderr, "Sorry that didn't work out. DefineDosDevice failed with error %d\n", err);
+		return 1;
+	}
+
+	return 0;
 }
 
 int main(int argc, char ** argv)
