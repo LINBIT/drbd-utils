df8a2c022ea17a73bf2e3a2546f72248ba863e26
Johannes Thoma <johannes@johannesthoma.com>
2018-01-24 18:26:38 +0100

    Very simple command line parsing.

--- user/windrbd/windrbd.c
+++ user/windrbd/windrbd.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int assign_drive_letter(int minor, const char *drive)
+{
+	printf("Assigning drive letter %s to minor %d\n", drive, minor);
+
+	return 0;
+}
+
+void usage_and_exit(void)
+{
+	fprintf(stderr, "Usage: windrbd assign-drive-letter <minor> <drive-letter>\n");
+	exit(1);
+}
 
 int main(int argc, char ** argv)
 {
-	printf("windrbd utility.\n");
+	const char *op;
 
+	if (argc < 2) {
+		usage_and_exit();
+	}
+	op = argv[1];
+	if (strcmp(op, "assign-drive-letter") == 0) {
+		if (argc != 4) {
+			usage_and_exit();
+		}
+		int minor = atoi(argv[2]);
+		const char *drive = argv[3];
+
+		return assign_drive_letter(minor, drive);
+	}
+
+	usage_and_exit();
 	return 0;
 }
