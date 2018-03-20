49de9a6077766d19fdba585065fdd5825b75aeb6
Johannes Thoma <johannes@johannesthoma.com>
2018-03-20 17:12:43 +0100

    drbdadm: allow GUIDs for windrbd show/hide filesystem.

--- user/v9/drbdadm_main.c
+++ user/v9/drbdadm_main.c
@@ -1124,11 +1124,9 @@ static int adm_attach(const struct cfg_ctx *ctx)
 			return rv;
 
 #ifdef WINDRBD
-		if (is_driveletter(vol->disk)) {
-			rv = call_windrbd(ctx->res->name, windrbd, "-q", "hide-filesystem", vol->disk, NULL);
-			if (rv)
-				return rv;
-		}
+		rv = call_windrbd(ctx->res->name, windrbd, "-q", "hide-filesystem", vol->disk, NULL);
+		if (rv)
+			return rv;
 #endif
 	}
 
@@ -1419,9 +1417,7 @@ static void __adm_drbdsetup(const struct cfg_ctx *ctx, int flags, pid_t *pid, in
 
 #ifdef WINDRBD
 	if (*ex == 0 && ctx->cmd == &detach_cmd) {
-		if (is_driveletter(ctx->vol->disk)) {
-			*ex = call_windrbd(ctx->res->name, windrbd, "-q", "show-filesystem", ctx->vol->disk, NULL);
-		}
+		*ex = call_windrbd(ctx->res->name, windrbd, "-q", "show-filesystem", ctx->vol->disk, NULL);
 	}
 #endif
 }
