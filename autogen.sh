fa1d7366f229c0f0123389ec91676190e6b6d13b
Roland Kammerer <roland.kammerer@linbit.com>
2018-11-26 16:17:41 +0100

    build: reorder output of autogen.sh
    I'm too used to copy/paste the last line.

--- autogen.sh
+++ autogen.sh
@@ -9,10 +9,10 @@ autoconf
 
 echo "
 suggested configure parameters:
+# for windrbd (on a cygwin host, assuming C:\windrbd is your windrbd root)
+./configure --prefix=/cygdrive/c/windrbd/usr --localstatedir=/cygdrive/c/windrbd/var --sysconfdir=/cygdrive/c/windrbd/etc --without-83support --without-84support --without-drbdmon --with-windrbd
 # prepare for rpmbuild, only generate spec files
 ./configure --enable-spec
 # or prepare for direct build
 ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc
-# or for windrbd (on a cygwin host, assuming C:\windrbd is your windrbd root)
-./configure --prefix=/cygdrive/c/windrbd/usr --localstatedir=/cygdrive/c/windrbd/var --sysconfdir=/cygdrive/c/windrbd/etc --without-83support --without-84support --without-drbdmon --with-windrbd
 "
