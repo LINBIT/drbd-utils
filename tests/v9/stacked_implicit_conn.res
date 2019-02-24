LINBIT/drbd-utils

commit 1263325b20362d14aa8f61c436da10b65a47d699

drbdadm: Generalize paths for in stacked resources
In the connection section of a stacked resource one can reference to
host sections of the lower resources. This is nothing new.

With that it is possible to create pathes from lower hosts that are
not local to this machine. Consider that by introducing an ignore
flag on the path.

consider this config excerpt:

	stacked-on-top-of site1 {
		node-id 0;
	}
	stacked-on-top-of site2 {
		node-id 1;
	}
	stacked-on-top-of site3 {
		node-id 2;
	}

	connection { # site1 - site2
		path {
			host alpha address 192.168.23.21:7100;
			host charlie address 192.168.24.21:7100;
		}
		path {
			host bravo address 192.168.23.22:7100;
			host delta address 192.168.24.22:7100;
		}
		path {
			host alpha address 192.168.23.21:7100;
			host delta address 192.168.24.22:7100;
		}
		path {
			host bravo address 192.168.23.22:7100;
			host charlie address 192.168.24.21:7100;
		}
	}

alpha and bravo are hosts of resource site1. chalie and delta are
hosts of resource site2. Now, when bringing up the paths on
alpha the paths from bravo need to be ingroned.

user/v9/drbdadm_postparse.c
@@ -365,6 +365,12 @@ void set_me_in_resource(struct d_resource* res, int match_on_proxy)
 			}
 
 			if (h) {
+				/* In case this is a path of a stacked resource ignore it if
+				   the path belongs to the "non local" node of the lower resource */
+				if (h->host_info->lower &&
+				    !hostname_in_list(h->name, &h->host_info->lower->me->on_hosts))
+					path->ignore = 1;
+
 				h->used_as_me = 1;
 				if (!path->my_address)
 					path->my_address = h->address.addr ? &h->address : &res->me->address;

user/v9/drbdadm.h
@@ -172,6 +172,7 @@ struct path
 	unsigned int implicit:1;
 	unsigned int adj_seen:1;
 	unsigned int proxy_conn_is_down:1;
+	unsigned int ignore:1;
 	STAILQ_ENTRY(path) link;
 };
 STAILQ_HEAD(paths, path);

user/v9/drbdadm_adjust.c
@@ -214,6 +214,8 @@ static bool adjust_paths(const struct cfg_ctx *ctx, struct connection *running_c
 	bool del_path = false;
 
 	for_each_path(configured_path, &configured_conn->paths) {
+		if (configured_path->ignore)
+			continue;
 		running_path = find_path_by_addrs(running_conn, configured_path);
 		if (!running_path) {
 			tmp_ctx.path = configured_path;

user/v9/drbdadm_main.c
@@ -611,6 +611,8 @@ static int __call_cmd_fn(const struct cfg_ctx *ctx, enum on_error on_error)
 		struct path *path;
 
 		for_each_path(path, &tmp_ctx.conn->paths) {
+			if (path->ignore)
+				continue;
 			tmp_ctx.path = path;
 			rv = tmp_ctx.cmd->function(&tmp_ctx);
 			if (rv >= 20) {

commit 0acf140eccf14aed59970818de1af03c506684fc

Do not ignore stacked/upper implicit paths
Commit 1263325b20 introduced a regression for connections (and paths)
of upper/stacked resources, that are created implicitly.
Since the mentioned commit they get always ignored, since they contain a
fakes hostname. Fix that by considering that when ignoring paths.

The problem for users was that they got peers without paths configured.

user/v9/drbdadm_postparse.c
@@ -366,8 +366,9 @@ void set_me_in_resource(struct d_resource* res, int match_on_proxy)
 
 			if (h) {
 				/* In case this is a path of a stacked resource ignore it if
-				   the path belongs to the "non local" node of the lower resource */
-				if (h->host_info->lower &&
+				   the path belongs to the "non local" node of the lower resource.
+				   Never ignore an implicit path, since they have faked hostnames */
+				if (!h->faked_hostname && h->host_info->lower &&
 				    !hostname_in_list(h->name, &h->host_info->lower->me->on_hosts))
 					path->ignore = 1;
 

tests/v9/stacked_implicit_conn.res
@@ -1,0 +1,46 @@
+global {
+  disable-ip-verification;
+}
+
+resource r0 {
+  net {
+    protocol C;
+  }
+
+  startup {
+        wfc-timeout             60;
+        degr-wfc-timeout        60;
+  }
+
+  on undertest {
+    device     /dev/drbd0;
+    disk       /dev/sdb1;
+    address    10.56.84.138:7788;
+    meta-disk  internal;
+  }
+
+  on node_b {
+    device    /dev/drbd0;
+    disk      /dev/sdb1;
+    address   10.56.84.139:7788;
+    meta-disk internal;
+  }
+}
+
+resource r0-U {
+  net {
+    protocol B;
+  }
+
+  stacked-on-top-of r0 {
+    device     /dev/drbd10;
+    address    10.56.84.142:7788;
+  }
+
+  on node_c {
+    device     /dev/drbd10;
+    disk       /dev/sdb1;
+    address    10.56.85.140:7788;
+    meta-disk  internal;
+  }
+}

tests/v9/stacked_implicit_conn.test
@@ -1,0 +1,9 @@
+$ drbdadm -c ../../tests/v9/stacked_implicit_conn.res -d up r0-U 2>/dev/null; echo $?
+drbdsetup new-resource r0-U 1
+drbdsetup new-minor r0-U 10 0
+drbdsetup new-peer r0-U 0 --_name=node_c --protocol=B
+drbdsetup new-path r0-U 0 ipv4:10.56.84.142:7788 ipv4:10.56.85.140:7788
+drbdmeta 10 v09 /dev/drbd0 internal apply-al
+drbdsetup attach 10 /dev/drbd0 /dev/drbd0 internal
+drbdsetup connect r0-U 0
+0