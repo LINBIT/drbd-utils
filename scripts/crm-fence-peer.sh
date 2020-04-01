LINBIT/drbd-utils

commit fd2eec6eca91e3d4d24852b5bac4dd61f9854e67

crm-fence-peer: detect pacemaker 2 "promotable clones"
The sed-script to guess the pacemaker xml-id to be used in location
constraints from the DRBD resource name expected '<master' tags, but
with new pacemaker 2 xml, these are '<clone's, with "promotable=true"
set in the meta attributes.

Use the crm_feature_set version from the top level <cib/> tag
to decide between the two.

scripts/crm-fence-peer.sh
@@ -51,15 +51,28 @@ fence_peer_init()
 {
 	# we know which instance we are: $OCF_RESOURCE_INSTANCE.
 	# but we do not know the xml ID of the <master/> :(
+
+	# with Pacemaker 2, its "promotable clones" instead of the
+	# "master" (which was deemed a bad naming choice).
+	# detect older pacemaker by crm_feature_set < 3.1.0
+	local clone_or_master=clone
+	case $crm_feature_set in
+		3.0.*|[012].*)	: "pacemaker version < 2, master slave"
+			clone_or_master=master ;;
+	esac
+
 	# cibadmin -Ql --xpath \
 	# '//master[primitive[@type="drbd" and instance_attributes/nvpair[@name = "drbd_resource" and @value="r0"]]]/@id'
 	# but I'd have to pipe that through sed anyways, because @attribute
 	# xpath queries are not supported.
 	# and I'd be incompatible with older cibadmin not supporting --xpath.
-	# be cool, sed it out:
+	# be cool, sed it out.
+	# I could be more strict about primitive class:provider:type,
+	# or double check that it is in fact a promotable="true" clone...
+	# But in the real world, this is good enough.
 	: ${master_id=$(set +x; echo "$cib_xml" |
-		sed -ne '/<master /,/<\/master>/ {
-			   /<master / h;
+		sed -ne "/<$clone_or_master /,/<\\/$clone_or_master>/ {
+			   /<$clone_or_master / h;"'
 			     /<primitive/,/<\/primitive/ {
 			       /<instance_attributes/,/<\/instance_attributes/ {
 				 /<nvpair .*\bname="drbd_resource"/ {

scripts/crm-fence-peer.9.sh
@@ -197,15 +197,28 @@ fence_peer_init()
 {
 	# we know which instance we are: $OCF_RESOURCE_INSTANCE.
 	# but we do not know the xml ID of the <master/> :(
+
+	# with Pacemaker 2, its "promotable clones" instead of the
+	# "master" (which was deemed a bad naming choice).
+	# detect older pacemaker by crm_feature_set < 3.1.0
+	local clone_or_master=clone
+	case $crm_feature_set in
+		3.0.*|[012].*)	: "pacemaker version < 2, master slave"
+			clone_or_master=master ;;
+	esac
+
 	# cibadmin -Ql --xpath \
 	# '//master[primitive[@type="drbd" and instance_attributes/nvpair[@name = "drbd_resource" and @value="r0"]]]/@id'
 	# but I'd have to pipe that through sed anyways, because @attribute
 	# xpath queries are not supported.
 	# and I'd be incompatible with older cibadmin not supporting --xpath.
-	# be cool, sed it out:
+	# be cool, sed it out.
+	# I could be more strict about primitive class:provider:type,
+	# or double check that it is in fact a promotable="true" clone...
+	# But in the real world, this is good enough.
 	: ${master_id=$(set +x; echo "$cib_xml" |
-		sed -ne '/<master /,/<\/master>/ {
-			   /<master / h;
+		sed -ne "/<$clone_or_master /,/<\\/$clone_or_master>/ {
+			   /<$clone_or_master / h;"'
 			     /<primitive/,/<\/primitive/ {
 			       /<instance_attributes/,/<\/instance_attributes/ {
 				 /<nvpair .*\bname="drbd_resource"/ {

commit 5e139c418719166c562ad49a2005465bc1a25442

fix crm-fence-peer.sh regression: define crm_feture_set
fixes pacemaker 1.x compat,
regression introduced by fd2eec6eca91e3d4d24852b5bac4dd61f9854e67

scripts/crm-fence-peer.sh
@@ -39,9 +39,28 @@ remove_constraint()
 }
 
 cib_xml=""
+cib_xml_first_line=""
+crm_feature_set=""
+admin_epoch=""
+epoch=""
+num_updates=""
+have_quorum=""
 get_cib_xml() {
 	cibadmin_invocations=$(( $cibadmin_invocations + 1 ))
 	cib_xml=$( set +x; cibadmin "$@" )
+	cib_xml_first_line=${cib_xml%%>*}
+
+	set -- ${cib_xml_first_line}
+	local x
+	for x ; do
+	case $x in
+	crm_feature_set=*)	x=${x#*'="'}; x=${x%'"'}; crm_feature_set=$x ;;
+	admin_epoch=*)		x=${x#*'="'}; x=${x%'"'}; admin_epoch=$x ;;
+	epoch=*)		x=${x#*'="'}; x=${x%'"'}; epoch=$x ;;
+	num_updates=*)		x=${x#*'="'}; x=${x%'"'}; num_updates=$x ;;
+	have-quorum=*)		x=${x#*'="'}; x=${x%'"'}; have_quorum=$x ;;
+	esac
+	done
 }
 
 