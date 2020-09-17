LINBIT/drbd-utils

commit 09c5ee3675136f57d0dd3102247da83cdb1c59b0

drbdsetup,events2: keep track of tree of objects
This makes it possible to compute additional statistics based on
multiple objects.

Statistics are now only printed when they have changed, so the matching
test has been adjusted.

Co-developed-by: Philipp Reisner <philipp.reisner@linbit.com>

tests/v9/events2-all-create-change.test
@@ -18,10 +18,10 @@ create device name:some-resource volume:0 minor:1000 disk:Diskless client:no quo
 create connection name:some-resource peer-node-id:1 conn-name:some-peer connection:StandAlone role:Unknown congested:no ap-in-flight:10 rs-in-flight:20
 create peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Off peer-disk:DUnknown peer-client:no resync-suspended:no received:5 sent:10 out-of-sync:25 pending:30 unacked:40
 create path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:no
-change device name:some-resource volume:0 minor:1000 disk:UpToDate client:no quorum:yes size:5 read:10 written:15 al-writes:40 bm-writes:50 upper-pending:60 lower-pending:70
+change device name:some-resource volume:0 minor:1000 disk:UpToDate client:no quorum:yes
 change path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:yes
-change connection name:some-resource peer-node-id:1 conn-name:some-peer connection:Connected ap-in-flight:10 rs-in-flight:20
-change peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Established peer-disk:UpToDate peer-client:no received:5 sent:10 out-of-sync:25 pending:30 unacked:40
+change connection name:some-resource peer-node-id:1 conn-name:some-peer connection:Connected
+change peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Established peer-disk:UpToDate peer-client:no
 change resource name:some-resource role:Primary
 0
 $ cat events2-all-create-change.msgs | drbdsetup_events2_instrumented --color | cat -v; echo $?

user/v9/drbdsetup.c
@@ -3395,7 +3395,7 @@ struct devices_list *new_device_from_info(struct genl_info *info)
 
 	d = calloc(1, sizeof(*d));
 
-	d->minor =  ((struct drbd_genlmsghdr*)(info->userhdr))->minor;
+	d->minor = ((struct drbd_genlmsghdr*)(info->userhdr))->minor;
 	d->ctx = ctx;
 	if (disk_conf_nl) {
 		int size = nla_total_size(nla_len(disk_conf_nl));
@@ -3581,7 +3581,10 @@ void free_connections(struct connections_list *connections)
 	while (connections) {
 		struct connections_list *l = connections;
 		connections = connections->next;
+		free(l->path_list);
 		free(l->net_conf);
+		free_peer_devices(l->peer_devices);
+		free_paths(l->paths);
 		free(l);
 	}
 }
@@ -3665,6 +3668,34 @@ void free_peer_devices(struct peer_devices_list *peer_devices)
 		struct peer_devices_list *p = peer_devices;
 		peer_devices = peer_devices->next;
 		free(p->peer_device_conf);
+		free(p);
+	}
+}
+
+struct paths_list *new_path_from_info(struct genl_info *info)
+{
+	struct drbd_cfg_context ctx = { .ctx_volume = -1U, .ctx_peer_node_id = -1U };
+	struct paths_list *p;
+
+	drbd_cfg_context_from_attrs(&ctx, info);
+	if (!ctx.ctx_resource_name)
+		return NULL;
+
+	p = calloc(1, sizeof(*p));
+	if (!p)
+		exit(20);
+
+	p->ctx = ctx;
+	drbd_path_info_from_attrs(&p->info, info);
+
+	return p;
+}
+
+void free_paths(struct paths_list *paths)
+{
+	while (paths) {
+		struct paths_list *p = paths;
+		paths = paths->next;
 		free(p);
 	}
 }

user/v9/drbdsetup.h
@@ -75,6 +75,9 @@ struct resources_list {
 	struct nlattr *res_opts;
 	struct resource_info info;
 	struct resource_statistics statistics;
+	bool destroyed; /* only used by events2 */
+	struct devices_list *devices; /* only used by events2 */
+	struct connections_list *connections; /* only used by events2 */
 };
 struct devices_list {
 	struct devices_list *next;
@@ -92,6 +95,8 @@ struct connections_list {
 	struct nlattr *net_conf;
 	struct connection_info info;
 	struct connection_statistics statistics;
+	struct peer_devices_list *peer_devices; /* only used by events2 */
+	struct paths_list *paths; /* only used by events2 */
 };
 struct peer_devices_list {
 	struct peer_devices_list *next;
@@ -101,6 +106,11 @@ struct peer_devices_list {
 	struct peer_device_statistics statistics;
 	struct devices_list *device;
 	int timeout_ms; /* used only by wait_for_family() */
+};
+struct paths_list {
+	struct paths_list *next;
+	struct drbd_cfg_context ctx;
+	struct drbd_path_info info;
 };
 
 typedef
@@ -146,10 +156,12 @@ struct resources_list *new_resource_from_info(struct genl_info *info);
 struct devices_list *new_device_from_info(struct genl_info *info);
 struct connections_list *new_connection_from_info(struct genl_info *info);
 struct peer_devices_list *new_peer_device_from_info(struct genl_info *info);
+struct paths_list *new_path_from_info(struct genl_info *info);
 void free_resources(struct resources_list *);
 void free_devices(struct devices_list *);
 void free_connections(struct connections_list *);
 void free_peer_devices(struct peer_devices_list *);
+void free_paths(struct paths_list *);
 
 int drbdsetup_main(int argc, char **argv);
 

commit 74b0bd985210c85bcd29bbf1880bd2cbad3742c5

drbdsetup,events2: print resync done value even without statistics option
This fixes a regression introduced in commit:
09c5ee36 drbdsetup,events2: keep track of tree of objects

The function 'print_peer_device_statistics' needs to be called even when
'opt_statistics' is not set because it is responsible for printing the
resync done percentage.

Also add a unit test for this behavior.

user/v9/drbdsetup_events2.c
@@ -691,9 +691,8 @@ static void print_peer_device_changes(const char *prefix, const char *action_new
 		new_peer_device->info.peer_resync_susp_user != old_peer_device->info.peer_resync_susp_user ||
 		new_peer_device->info.peer_resync_susp_peer != old_peer_device->info.peer_resync_susp_peer ||
 		new_peer_device->info.peer_resync_susp_dependency != old_peer_device->info.peer_resync_susp_dependency;
-	statistics_changed = opt_statistics &&
-		(!old_peer_device ||
-		 memcmp(&new_peer_device->statistics, &old_peer_device->statistics, sizeof(struct peer_device_statistics)));
+	statistics_changed = !old_peer_device ||
+		 memcmp(&new_peer_device->statistics, &old_peer_device->statistics, sizeof(struct peer_device_statistics));
 
 	if (!repl_state_changed && !disk_changed && !resync_suspended_changed && !statistics_changed)
 		return;

tests/v9/events2-all-create-change.test
@@ -18,7 +18,7 @@ exists -
 create resource name:some-resource role:Secondary suspended:no write-ordering:none may_promote:no promotion_score:0
 create device name:some-resource volume:0 minor:1000 disk:Diskless client:no quorum:yes size:5 read:10 written:15 al-writes:40 bm-writes:50 upper-pending:60 lower-pending:70 al-suspended:no blocked:no
 create connection name:some-resource peer-node-id:1 conn-name:some-peer connection:StandAlone role:Unknown congested:no ap-in-flight:10 rs-in-flight:20
-create peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Off peer-disk:DUnknown peer-client:no resync-suspended:no received:5 sent:10 out-of-sync:25 pending:30 unacked:40
+create peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Off peer-disk:DUnknown peer-client:no resync-suspended:no received:5 sent:10 out-of-sync:0 pending:30 unacked:40
 create path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:no
 change resource name:some-resource may_promote:yes promotion_score:10101
 change device name:some-resource volume:0 minor:1000 disk:UpToDate client:no quorum:yes

tests/v9/events2-sync.msgs
@@ -1,0 +1,10 @@
+initial_state_done
+resource_create
+device_create
+connection_create
+peer_device_create
+path_create
+device_change_disk
+path_change_established
+connection_change_connection
+peer_device_change_sync

tests/v9/events2-sync.test
@@ -1,0 +1,26 @@
+$ cat events2-sync.msgs | drbdsetup_events2_instrumented; echo $?
+exists -
+create resource name:some-resource role:Secondary suspended:no may_promote:no promotion_score:0
+create device name:some-resource volume:0 minor:1000 disk:Diskless client:no quorum:yes
+create connection name:some-resource peer-node-id:1 conn-name:some-peer connection:StandAlone role:Unknown
+create peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Off peer-disk:DUnknown peer-client:no resync-suspended:no
+create path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:no
+change resource name:some-resource may_promote:yes promotion_score:10101
+change device name:some-resource volume:0 minor:1000 disk:UpToDate client:no quorum:yes
+change path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:yes
+change connection name:some-resource peer-node-id:1 conn-name:some-peer connection:Connected role:Secondary
+change peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:SyncSource peer-disk:Inconsistent peer-client:no done:37.50
+0
+$ cat events2-sync.msgs | drbdsetup_events2_instrumented --statistics; echo $?
+exists -
+create resource name:some-resource role:Secondary suspended:no write-ordering:none may_promote:no promotion_score:0
+create device name:some-resource volume:0 minor:1000 disk:Diskless client:no quorum:yes size:5 read:10 written:15 al-writes:40 bm-writes:50 upper-pending:60 lower-pending:70 al-suspended:no blocked:no
+create connection name:some-resource peer-node-id:1 conn-name:some-peer connection:StandAlone role:Unknown congested:no ap-in-flight:10 rs-in-flight:20
+create peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:Off peer-disk:DUnknown peer-client:no resync-suspended:no received:5 sent:10 out-of-sync:0 pending:30 unacked:40
+create path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:no
+change resource name:some-resource may_promote:yes promotion_score:10101
+change device name:some-resource volume:0 minor:1000 disk:UpToDate client:no quorum:yes
+change path name:some-resource peer-node-id:1 conn-name:some-peer local:ipv4:1.2.3.4:7789 peer:ipv4:5.6.7.8:7790 established:yes
+change connection name:some-resource peer-node-id:1 conn-name:some-peer connection:Connected role:Secondary
+change peer-device name:some-resource peer-node-id:1 conn-name:some-peer volume:0 replication:SyncSource peer-disk:Inconsistent peer-client:no done:37.50 received:5 sent:10 out-of-sync:2500 pending:30 unacked:40 dbdt1:0.24 eta:10
+0

user/v9/test/drbdsetup_events2_instrumented.c
@@ -174,17 +174,17 @@ void test_peer_device_info(struct msg_buff *smsg, __u32 peer_repl_state, __u32 p
 	nla_nest_end(smsg, nla);
 }
 
-void test_peer_device_statistics(struct msg_buff *smsg)
+void test_peer_device_statistics(struct msg_buff *smsg, bool resync)
 {
 	struct nlattr *nla = nla_nest_start(smsg, DRBD_NLA_PEER_DEVICE_STATISTICS);
 	nla_put_u64(smsg, T_peer_dev_received, 10);
 	nla_put_u64(smsg, T_peer_dev_sent, 20);
 	nla_put_u32(smsg, T_peer_dev_pending, 30);
 	nla_put_u32(smsg, T_peer_dev_unacked, 40);
-	nla_put_u64(smsg, T_peer_dev_out_of_sync, 50);
+	nla_put_u64(smsg, T_peer_dev_out_of_sync, resync ? 5000 : 0);
 	nla_put_u64(smsg, T_peer_dev_resync_failed, 0);
 	nla_put_u32(smsg, T_peer_dev_flags, 0);
-	nla_put_u64(smsg, T_peer_dev_rs_total, 0);
+	nla_put_u64(smsg, T_peer_dev_rs_total, resync ? 8000 : 0);
 	nla_put_u64(smsg, T_peer_dev_ov_start_sector, 0);
 	nla_put_u64(smsg, T_peer_dev_ov_stop_sector, 0);
 	nla_put_u64(smsg, T_peer_dev_ov_position, 0);
@@ -195,8 +195,8 @@ void test_peer_device_statistics(struct msg_buff *smsg)
 	nla_put_u64(smsg, T_peer_dev_rs_paused_ms, 0);
 	nla_put_u64(smsg, T_peer_dev_rs_dt0_ms, 0);
 	nla_put_u64(smsg, T_peer_dev_rs_db0_sectors, 0);
-	nla_put_u64(smsg, T_peer_dev_rs_dt1_ms, 0);
-	nla_put_u64(smsg, T_peer_dev_rs_db1_sectors, 0);
+	nla_put_u64(smsg, T_peer_dev_rs_dt1_ms, resync ? 100 : 0);
+	nla_put_u64(smsg, T_peer_dev_rs_db1_sectors, resync ? 50 : 0);
 	nla_put_u32(smsg, T_peer_dev_rs_c_sync_rate, 0);
 	nla_nest_end(smsg, nla);
 }
@@ -385,7 +385,7 @@ void test_peer_device_create(struct msg_buff *smsg)
 	test_peer_device_context(smsg);
 	test_notification_header(smsg, NOTIFY_CREATE);
 	test_peer_device_info(smsg, L_OFF, D_UNKNOWN);
-	test_peer_device_statistics(smsg);
+	test_peer_device_statistics(smsg, false);
 }
 
 void test_peer_device_change_replication(struct msg_buff *smsg)
@@ -394,7 +394,16 @@ void test_peer_device_change_replication(struct msg_buff *smsg)
 	test_peer_device_context(smsg);
 	test_notification_header(smsg, NOTIFY_CHANGE);
 	test_peer_device_info(smsg, L_ESTABLISHED, D_UP_TO_DATE);
-	test_peer_device_statistics(smsg);
+	test_peer_device_statistics(smsg, false);
+}
+
+void test_peer_device_change_sync(struct msg_buff *smsg)
+{
+	test_msg_put(smsg, DRBD_PEER_DEVICE_STATE, -1U);
+	test_peer_device_context(smsg);
+	test_notification_header(smsg, NOTIFY_CHANGE);
+	test_peer_device_info(smsg, L_SYNC_SOURCE, D_INCONSISTENT);
+	test_peer_device_statistics(smsg, true);
 }
 
 void test_peer_device_destroy(struct msg_buff *smsg)
@@ -493,6 +502,7 @@ int test_build_msg(struct msg_buff *smsg, char *msg_name)
 	TEST_MSG(connection_destroy);
 	TEST_MSG(peer_device_create);
 	TEST_MSG(peer_device_change_replication);
+	TEST_MSG(peer_device_change_sync);
 	TEST_MSG(peer_device_destroy);
 	TEST_MSG(path_create);
 	TEST_MSG(path_change_established);