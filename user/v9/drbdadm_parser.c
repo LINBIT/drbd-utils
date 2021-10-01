LINBIT/drbd-utils

commit 2880596fec8874b65a7b5791bbc6b22c00862fbe

drbdadm: Enforce maximum field with for strings when generating drbdsetup commands
In stacked setups, it generates concaternated hostnames of the lower hosts for the
peer connection name in the upper layer. That might exceed the 64 character limit.

It does not hurt to simply truncate that at 64 characters. Implement that in a
way that we would never output a too long string onto the drbdsetup command line.

user/v9/drbdadm_parser.c
@@ -539,8 +539,8 @@ static void pe_options(struct context_def *options_def)
 	pe_expected(buffer);
 }
 
-static struct field_def *find_field(bool *no_prefix, struct context_def *options_def,
-				    const char *name)
+struct field_def *find_field(bool *no_prefix, const struct context_def *options_def,
+			     const char *name)
 {
 	struct field_def *field;
 	bool ignored_no_prefix;

user/v9/config_flags.c
@@ -739,7 +739,7 @@ struct field_class fc_string = {
 
 #define STRING_MAX_LEN(f, l)								\
 	STRING(f),									\
-	.u = { .s = { .max_len = l } } }
+	.u = { .s = { .max_len = l } }
 
 #define ENUM_NUM(f, d, num_min, num_max)			\
 	.nla_type = T_ ## f,					\
@@ -874,8 +874,8 @@ const struct en_map quorum_map[] = {
           .unit = "bytes" },								\
 	{ "ko-count", NUMERIC(ko_count, KO_COUNT) },					\
 	{ "allow-two-primaries", BOOLEAN(two_primaries, ALLOW_TWO_PRIMARIES) },		\
-	{ "cram-hmac-alg", STRING(cram_hmac_alg) },					\
-	{ "shared-secret", STRING_MAX_LEN(shared_secret, SHARED_SECRET_MAX), 		\
+	{ "cram-hmac-alg", STRING_MAX_LEN(cram_hmac_alg, SHARED_SECRET_MAX) },		\
+	{ "shared-secret", STRING_MAX_LEN(shared_secret, SHARED_SECRET_MAX) },		\
 	{ "after-sb-0pri", ENUM(after_sb_0p, AFTER_SB_0P) },				\
 	{ "after-sb-1pri", ENUM(after_sb_1p, AFTER_SB_1P) },				\
 	{ "after-sb-2pri", ENUM(after_sb_2p, AFTER_SB_2P) },				\
@@ -883,22 +883,22 @@ const struct en_map quorum_map[] = {
 	{ "rr-conflict", ENUM(rr_conflict, RR_CONFLICT) },				\
 	{ "ping-timeout", NUMERIC(ping_timeo, PING_TIMEO),				\
           .unit = "1/10 seconds" },							\
-	{ "data-integrity-alg", STRING(integrity_alg) },				\
+	{ "data-integrity-alg", STRING_MAX_LEN(integrity_alg, SHARED_SECRET_MAX) },	\
 	{ "tcp-cork", BOOLEAN(tcp_cork, TCP_CORK) },					\
 	{ "on-congestion", ENUM(on_congestion, ON_CONGESTION) },			\
 	{ "congestion-fill", NUMERIC(cong_fill, CONG_FILL),				\
           .unit = "bytes" },								\
 	{ "congestion-extents", NUMERIC(cong_extents, CONG_EXTENTS) },			\
-	{ "csums-alg", STRING(csums_alg) },						\
+	{ "csums-alg", STRING_MAX_LEN(csums_alg, SHARED_SECRET_MAX) },			\
 	{ "csums-after-crash-only", BOOLEAN(csums_after_crash_only,			\
 						CSUMS_AFTER_CRASH_ONLY) },		\
-	{ "verify-alg", STRING(verify_alg) },						\
+	{ "verify-alg", STRING_MAX_LEN(verify_alg, SHARED_SECRET_MAX) },		\
 	{ "use-rle", BOOLEAN(use_rle, USE_RLE) },					\
 	{ "socket-check-timeout", NUMERIC(sock_check_timeo, SOCKET_CHECK_TIMEO) },	\
 	{ "fencing", ENUM(fencing_policy, FENCING) },					\
 	{ "max-buffers", NUMERIC(max_buffers, MAX_BUFFERS) },				\
 	{ "allow-remote-read", BOOLEAN(allow_remote_read, ALLOW_REMOTE_READ) },					\
-	{ "_name", STRING(name) }
+	{ "_name", STRING_MAX_LEN(name, SHARED_SECRET_MAX) }
 
 struct context_def disk_options_ctx = {
 	NLA_POLICY(disk_conf),
@@ -951,7 +951,7 @@ struct context_def new_peer_cmd_ctx = {
 	NLA_POLICY(net_conf),
 	.nla_type = DRBD_NLA_NET_CONF,
 	.fields = {
-		{ "transport", STRING(transport_name) },
+		{ "transport", STRING_MAX_LEN(transport_name, SHARED_SECRET_MAX) },
 		CHANGEABLE_NET_OPTIONS,
 		{ } },
 };
@@ -1008,7 +1008,7 @@ struct context_def resource_options_ctx = {
 	NLA_POLICY(res_opts),
 	.nla_type = DRBD_NLA_RESOURCE_OPTS,
 	.fields = {
-		{ "cpu-mask", STRING(cpu_mask) },
+		{ "cpu-mask", STRING_MAX_LEN(cpu_mask, DRBD_CPU_MASK_SIZE) },
 		{ "on-no-data-accessible", ENUM(on_no_data, ON_NO_DATA) },
 		{ "auto-promote", BOOLEAN(auto_promote, AUTO_PROMOTE) },
 		{ "peer-ack-window", NUMERIC(peer_ack_window, PEER_ACK_WINDOW), .unit = "bytes" },

user/v9/drbdadm.h
@@ -444,6 +444,8 @@ int parse_proxy_options_section(struct d_proxy_info **proxy);
 struct peer_device *find_peer_device(struct connection *conn, int vnr);
 bool peer_diskless(struct peer_device *peer_device);
 
+struct field_def *find_field(bool *no_prefix, const struct context_def *options_def, const char *name);
+
 extern char *config_file;
 extern char *config_save;
 extern int config_valid;

user/v9/drbdadm_main.c
@@ -1111,7 +1111,7 @@ static void add_setup_options(char **argv, int *argcp, const struct context_def 
 	*argcp = argc;
 }
 
-#define make_option(ARG, OPT) do {						\
+#define make_option(ARG, OPT, OPTIONS_DEF) do {					\
 	struct d_name *b_opt; 							\
 	bool found = false; 							\
 	STAILQ_FOREACH(b_opt, &backend_options, link) {				\
@@ -1121,17 +1121,23 @@ static void add_setup_options(char **argv, int *argcp, const struct context_def 
 		} 								\
 	} 									\
 	if (!found) {								\
-		if(OPT->value)							\
-			ARG = ssprintf("--%s=%s", OPT->name, OPT->value);	\
-		else  								\
+		if(OPT->value) {						\
+			int w = 512;						\
+			struct field_def *field =				\
+				find_field(NULL, OPTIONS_DEF, OPT->name);	\
+			if (field && field->ops == &fc_string)			\
+				w = field->u.s.max_len;				\
+			ARG = ssprintf("--%s=%.*s", OPT->name, w, OPT->value);	\
+		} else {							\
 			ARG = ssprintf("--%s", OPT->name);			\
+		}								\
 	}									\
 } while (0)
 
-#define make_options(ARG, OPTIONS) do {					\
+#define make_options(ARG, OPTIONS, OPTIONS_DEF) do {			\
 	struct d_option *option;					\
 	STAILQ_FOREACH(option, OPTIONS, link) 				\
-		make_option(ARG, option);				\
+		make_option(ARG, option, OPTIONS_DEF);			\
 } while (0)
 
 #define ssprintf_addr(A)					\
@@ -1176,9 +1182,9 @@ static int adm_attach(const struct cfg_ctx *ctx)
 		argv[NA(argc)] = "--set-defaults";
 		STAILQ_FOREACH(option, &ctx->vol->disk_options, link)
 			if (!option->adj_skip)
-				make_option(argv[NA(argc)], option);
+				make_option(argv[NA(argc)], option, ctx->cmd->drbdsetup_ctx);
 	} else
-		make_options(argv[NA(argc)], &ctx->vol->disk_options);
+		make_options(argv[NA(argc)], &ctx->vol->disk_options, ctx->cmd->drbdsetup_ctx);
 	add_setup_options(argv, &argc, ctx->cmd->drbdsetup_ctx);
 	argv[NA(argc)] = 0;
 
@@ -1249,7 +1255,7 @@ static int adm_resource(const struct cfg_ctx *ctx)
 	if (reset)
 		argv[NA(argc)] = "--set-defaults";
 	if (reset || do_new_resource)
-		make_options(argv[NA(argc)], &res->res_options);
+		make_options(argv[NA(argc)], &res->res_options, ctx->cmd->drbdsetup_ctx);
 	add_setup_options(argv, &argc, ctx->cmd->drbdsetup_ctx);
 	argv[NA(argc)] = NULL;
 
@@ -1769,7 +1775,7 @@ int adm_peer_device(const struct cfg_ctx *ctx)
 	if (reset)
 		argv[NA(argc)] = "--set-defaults";
 
-	make_options(argv[NA(argc)], &peer_device->pd_options);
+	make_options(argv[NA(argc)], &peer_device->pd_options, ctx->cmd->drbdsetup_ctx);
 	add_setup_options(argv, &argc, ctx->cmd->drbdsetup_ctx);
 	argv[NA(argc)] = 0;
 
@@ -1815,7 +1821,7 @@ static int adm_new_peer(const struct cfg_ctx *ctx)
 	if (!strncmp(ctx->cmd->name, "net-options", 11))
 		del_opt(&conn->net_options, "transport");
 
-	make_options(argv[NA(argc)], &conn->net_options);
+	make_options(argv[NA(argc)], &conn->net_options, ctx->cmd->drbdsetup_ctx);
 
 	add_setup_options(argv, &argc, ctx->cmd->drbdsetup_ctx);
 	argv[NA(argc)] = 0;
@@ -2106,7 +2112,7 @@ static int adm_wait_c(const struct cfg_ctx *ctx)
 		argv[argc] = ssprintf("%lu", timeout);
 		argc++;
 	} else
-		make_options(argv[NA(argc)], &res->startup_options);
+		make_options(argv[NA(argc)], &res->startup_options, ctx->cmd->drbdsetup_ctx);
 	argv[NA(argc)] = 0;
 
 	rv = m_system_ex(argv, SLEEPS_FOREVER, res->name);
@@ -2505,7 +2511,7 @@ static int adm_wait_ci(const struct cfg_ctx *ctx)
 		argv[NA(argc)] = drbdsetup;
 		argv[NA(argc)] = "wait-connect-resource";
 		argv[NA(argc)] = res->name;
-		make_options(argv[NA(argc)], &res->startup_options);
+		make_options(argv[NA(argc)], &res->startup_options, ctx->cmd->drbdsetup_ctx);
 		argv[NA(argc)] = 0;
 
 		m__system(argv, RETURN_PID, res->name, &pids[i++], NULL, NULL);

commit 76e90f0cc456882ea6ca4ed36789f5e6508d850b

drbd: fix segfault of wait-* family sub commands
Recent regression in 9.18.2 introduced by
  2880596f drbdadm: Enforce maximum field with for strings when generating drbdsetup commands

That changed "make_options" to take a "drbdsetup_ctx" for default options.
For the "wait-*" family of functions, we do not define that context, the NULL
is passed down into find_field(), which then in an attempt to iterate over
the default option list tries to dereference it.

fix: in find_field(), check options_def against NULL first.

We may consider to still add option contexts to the "wait-*" sub-command definitions.

user/v9/drbdadm_parser.c
@@ -555,6 +555,9 @@ const struct field_def *find_field(bool *no_prefix, const struct context_def *op
 		*no_prefix = false;
 	}
 
+	if (!options_def)
+		return NULL;
+
 	for (field = options_def->fields; field->name; field++) {
 		if (!strcmp(field->name, name))
 			return field;