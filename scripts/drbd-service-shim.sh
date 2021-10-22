51227dcb3c1239bd5ebb01b771a6db0919ee3203
Lars Ellenberg <lars.ellenberg@linbit.com>
2021-10-22 16:38:31 +0200

    reactor/systemd: add "secondary-or-escalate"
    If "drbdsetup secondary" fails,
    a drbd-reactor based cluster would be blocked on this resource.
    
    Optionally use either `FailureAction` directly (if you can use `ExecCondition`
    for promotion) or `OnFailure=drbd-demote-or-escalate@%i.service` to
    escalate to `reboot` (or `reboot-immediate`) if you need this.
    
    For the paranoid setups, you'd need to have systemd use a WatchdogDevice,
    set RuntimeWatchdogSec, RebootWatchdogSec, and possibly set JobTimeoutSec
    and JobTimeoutAction on shutdown.target, reboot.target,
    and the drbd-demote-or-escalate@.service.

--- scripts/drbd-service-shim.sh
+++ scripts/drbd-service-shim.sh
@@ -20,6 +20,41 @@ primary)
 secondary)
   exec /usr/sbin/drbdsetup secondary "$2"
   ;;
+secondary-or-escalate)
+	# Log something and try to get journald to flush its logs
+	# to (hopefully) persistent storage, so we at least have some
+	# indication of why we rebooted -- if that turns out to be necessary.
+	echo >&2 "<6>about to demote (or escalate to the FailureAction)"
+	journalctl --flush --sync
+	/usr/sbin/drbdsetup secondary "$2"
+	ex_secondary=$?
+	case $ex_secondary in
+	 0)
+		# successfully demoted, already secondary anyways,
+		# or module is not even loaded
+		exit 0 ;;
+
+	# any other special treatment for special exit codes?
+	*)
+		# double check for "resource does not exist"
+		current_state=$(/usr/sbin/drbdsetup events2 --now "$2")
+		if [[ $current_state = "exists -" ]]; then
+			echo >&2 "<7>not even configured"
+			exit 0
+		fi
+		echo >&2 "<7>current state: ${current_state//$'\n'/ // }"
+
+		# TODO: optionally call some hook script,
+		# which can go crazy trying to make this resource "demotable".
+		# Then try again.
+		;;
+	esac
+
+	# if we fail due to timeout, this won't even be reached :-(
+	echo >&2 "<0>failed to demote, exit code=$ex_secondary; about to escalate to FailureAction"
+	exit $ex_secondary
+	;;
+
 *)
   echo "Unknown verb $1" >&2
   exit 1
