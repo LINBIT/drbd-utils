ef9a32ce9f63e6f8c48bd8f8585c1cf84d44d308
Robert Altnoeder <robert.altnoeder@linbit.com>
2024-02-09 01:06:06 +0100

    DRBDmon: Version V1R2M5

--- user/drbdmon/DrbdMonConsts.cpp
+++ user/drbdmon/DrbdMonConsts.cpp
@@ -7,6 +7,6 @@ extern "C"
 }
 
 const std::string DrbdMonConsts::PROGRAM_NAME       = "DRBDmon";
-const std::string DrbdMonConsts::PROJECT_VERSION    = "V1R2M4";
+const std::string DrbdMonConsts::PROJECT_VERSION    = "V1R2M5";
 const std::string DrbdMonConsts::UTILS_VERSION      = PACKAGE_VERSION;
 const std::string DrbdMonConsts::BUILD_HASH         = GITHASH;
