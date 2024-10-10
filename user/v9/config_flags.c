LINBIT/drbd-utils

commit 20d021dbc44869ca987bb15b08dcf95fb80dbb5e

configure.ac: add option for disabling keyutils
Some platforms (like Windows) do not have access to keyutils. Since it is only
used to make the configuration of TLS keys more convenient, we can swap the
implementation with a generic "numeric" one, as the DRBD API simply expects
a "key_serial_t", which is an alias for int32_t.

When not set, we try to detect the presence of keyutils and add support. If
explicitly requested, failure to find libkeyutils is an error.

user/v9/config_flags.c
@@ -6,7 +6,10 @@
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <linux/genetlink.h>
+
+#ifdef KEYUTILS
 #include <keyutils.h>
+#endif
 
 #include "libgenl.h"
 #include "linux/drbd.h"
@@ -694,6 +697,7 @@ struct field_class fc_string = {
 
 /* ---------------------------------------------------------------------------------------------- */
 
+#ifdef KEYUTILS
 static key_serial_t string_to_key_serial(const char *value, const char *key_type)
 {
 	if (!value)
@@ -776,7 +780,7 @@ struct field_class fc_key_serial = {
 	.describe_xml = key_serial_describe_xml,
 	.check = key_serial_check,
 };
-
+#endif
 
 /* ============================================================================================== */
 
@@ -839,11 +843,24 @@ struct field_class fc_key_serial = {
 		.max = num_max,					\
 		.def = DRBD_ ## d ## _DEF, } }			\
 
+#ifdef KEYUTILS
 #define KEY_SERIAL(f, key_type)					\
 	.nla_type = T_ ## f,					\
 	.ops = &fc_key_serial,					\
 	.u = { .k = {						\
 		.type = key_type, } }				\
+
+#else
+#define KEY_SERIAL(f, keytype)					\
+	.nla_type = T_ ## f,					\
+	.ops = &fc_numeric,					\
+	.u = { .n = {						\
+		.min = INT32_MIN,				\
+		.max = INT32_MAX,				\
+		.def = 0,					\
+		.is_signed = true,				\
+		.scale = 1 } }
+#endif
 /* ============================================================================================== */
 
 const char *wire_protocol_map[] = {

configure.ac
@@ -58,6 +58,7 @@ WITH_MANUAL=yes
 WITH_DRBDMON=yes
 WITH_WINDRBD=no
 WITH_PREBUILTMAN=no
+WITH_KEYUTILS=yes
 AC_ARG_WITH([84support],
 	[AS_HELP_STRING([--without-84support],
 			[Do not include support for drbd driver/module 8.4])],
@@ -116,6 +117,10 @@ AC_ARG_WITH([prebuiltman],
 	[AS_HELP_STRING([--with-prebuiltman],
 			[Do not try to (re)generate man pages])],
 	[WITH_PREBUILTMAN=$withval])
+AC_ARG_WITH([keyutils],
+	[AS_HELP_STRING([--with-keyutils],
+			[Enable keyring integration for DRBD TLS])],
+	[WITH_KEYUTILS=$withval])
 
 AC_ARG_WITH([windrbd],
 	[AS_HELP_STRING([--with-windrbd],
@@ -151,7 +156,23 @@ AC_CHECK_TYPES([__free_fn_t],[],[],
 )
 
 dnl Check for keyutils
-AC_SEARCH_LIBS([request_key],[keyutils],[],[AC_MSG_ERROR([Need keyutils])],[])
+if test x"$with_keyutils" != xno; then
+	AC_SEARCH_LIBS(
+		[request_key],
+		[keyutils],
+		[WITH_KEYUTILS=yes],
+		[
+			if test x"$with_keyutils" = xyes; then
+				AC_MSG_ERROR([Could not find keyutils])
+			else
+				AC_MSG_WARN([Could not find keyutils])
+				WITH_KEYUTILS=no
+			fi
+		],
+		[]
+	)
+fi
+AC_SUBST(WITH_KEYUTILS)
 
 dnl Do we know where systemd unit files go?
 PKG_PROG_PKG_CONFIG

user/v9/Makefile.in
@@ -53,6 +53,7 @@ WITH_RGMANAGER = @WITH_RGMANAGER@
 WITH_BASHCOMPLETION = @WITH_BASHCOMPLETION@
 WITH_WINDRBD = @WITH_WINDRBD@
 WITH_CLITEST = @WITH_CLITEST@
+WITH_KEYUTILS = @WITH_KEYUTILS@
 
 # for some reason some of the commands below only work correctly in bash,
 # and not in e.g. dash. I'm too lazy to fix it to be compatible.
@@ -68,6 +69,9 @@ endif
 CFLAGS += -Wall -I../../drbd-headers -I.. -I. -I../shared
 ifeq ($(WITH_WINDRBD),yes)
 CFLAGS += -I../windrbd-compat-headers -DWINDRBD
+endif
+ifeq ($(WITH_KEYUTILS),yes)
+CFLAGS += -DKEYUTILS
 endif
 CFLAGS += $(EXTRA_CFLAGS)
 

commit 2b2501524564778cb9b5bf332280b13e6e1facd5

config_flags: Fix the tls- options if keyutils are not available
On a Linux without keyutils, all drbd-utils were severely broken. Even
`drbdsetup show` aborted with cryptic failure messages like:
0 is not a valid unit

Fix it by introducing a dummy fc_key_serial class that is compatible
with the real thing.

Fixes: 20d021db ("configure.ac: add option for disabling keyutils")

user/v9/config_flags.c
@@ -771,6 +771,52 @@ static enum check_codes key_serial_check(const struct field_def *field, const ch
 	return CC_OK;
 }
 
+#else
+
+static bool key_serial_is_default(const struct field_def *field, const char *value)
+{
+	return true;
+}
+
+static bool key_serial_is_equal(const struct field_def *field, const char *a, const char *b)
+{
+	return true;
+}
+
+static const char *get_key_serial(struct context_def *ctx, const struct field_def *field, struct nlattr *nla)
+{
+	static char description[] = "\"\"";
+
+	return description;
+}
+
+static bool put_key_serial(struct context_def *ctx, const struct field_def *field,
+                       struct msg_buff *msg, const char *value)
+{
+	nla_put_u32(msg, field->nla_type, 0);
+	return true;
+}
+
+static int key_serial_usage(const struct field_def *field, char *str, int size)
+{
+	return snprintf(str, size,"[--%s=<n/a>]",
+	                field->name);
+}
+
+static void key_serial_describe_xml(const struct field_def *field)
+{
+	printf("\t<option name=\"%s\" type=\"string\">\n"
+	       "\t</option>\n",
+	       field->name);
+}
+
+static enum check_codes key_serial_check(const struct field_def *field, const char *value)
+{
+	return CC_OK;
+}
+
+#endif
+
 struct field_class fc_key_serial = {
 	.is_default = key_serial_is_default,
 	.is_equal = key_serial_is_equal,
@@ -780,7 +826,7 @@ struct field_class fc_key_serial = {
 	.describe_xml = key_serial_describe_xml,
 	.check = key_serial_check,
 };
-#endif
+
 
 /* ============================================================================================== */
 
@@ -843,24 +889,12 @@ struct field_class fc_key_serial = {
 		.max = num_max,					\
 		.def = DRBD_ ## d ## _DEF, } }			\
 
-#ifdef KEYUTILS
 #define KEY_SERIAL(f, key_type)					\
 	.nla_type = T_ ## f,					\
 	.ops = &fc_key_serial,					\
 	.u = { .k = {						\
 		.type = key_type, } }				\
 
-#else
-#define KEY_SERIAL(f, keytype)					\
-	.nla_type = T_ ## f,					\
-	.ops = &fc_numeric,					\
-	.u = { .n = {						\
-		.min = INT32_MIN,				\
-		.max = INT32_MAX,				\
-		.def = 0,					\
-		.is_signed = true,				\
-		.scale = 1 } }
-#endif
 /* ============================================================================================== */
 
 const char *wire_protocol_map[] = {