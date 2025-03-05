LINBIT/drbd-utils

commit 18f80de3d9ff0da105353a96a232a486397a6049

drbdsetup: add udev command
Add a new command to create environment descriptions for udev based on the
in-kernel description. This is a replacement for the drbadm "sh-udev" command,
which by comparison uses the drbdadm configuration.

This has a number of advantages:

* The udev configuration is always in sync with what is actually configured
  in the kernel: in the drbdadm configuration, a changed config file that was
  not fully applied would still result in updated udev links, even if those
  links are now wrong.
* There might be issues with parsing the drbdadm configuration: in this case,
  we could not generate any udev links: this is no longer an issue.
* A particular issue is the use of the hostname in configuration: because
  systemd-udev often starts before the hostname is configured and it uses
  ProtectHostname=, it oftentimes keeps the "generic" hostname, while DRBD
  is configured with the updated hostname. drbdadm then does not find the
  "correct" host in the configuration, generating no links at all.

user/v84/drbdsetup.c
@@ -208,6 +208,7 @@ static int cstate_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *
 static int dstate_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *u_ptr);
 static int uuids_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *u_ptr);
 static int lk_bdev_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *u_ptr);
+static int udev_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *u_ptr);
 static int print_broadcast_events(const struct drbd_cmd *, struct genl_info *, void *u_ptr);
 static int w_connected_state(const struct drbd_cmd *, struct genl_info *, void *u_ptr);
 static int w_synced_state(const struct drbd_cmd *, struct genl_info *, void *u_ptr);
@@ -417,7 +418,8 @@ const struct drbd_cmd commands[] = {
 	 .ctx = &new_minor_cmd_ctx },
 
 	{"del-minor", CTX_MINOR, DRBD_ADM_DEL_MINOR, NO_PAYLOAD, del_minor_cmd, },
-	{"del-resource", CTX_RESOURCE, DRBD_ADM_DEL_RESOURCE, NO_PAYLOAD, del_resource_cmd, }
+	{"del-resource", CTX_RESOURCE, DRBD_ADM_DEL_RESOURCE, NO_PAYLOAD, del_resource_cmd, },
+	{"udev", CTX_MINOR, F_GET_CMD(udev_scmd), NO_PAYLOAD, .lockless=true, }
 };
 
 bool show_defaults;
@@ -2309,6 +2311,29 @@ static int down_cmd(const struct drbd_cmd *cm, int argc, char **argv)
 		free_minors(minors);
 		return print_config_error(rv, NULL);
 	}
+	return 0;
+}
+
+static int udev_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *u_ptr)
+{
+	struct drbd_cfg_context cfg = { .ctx_volume = -1U };
+	struct disk_conf dc = { .disk_size = 0, };
+
+	if (!info) {
+		return 1;
+	}
+
+	drbd_cfg_context_from_attrs(&cfg, info);
+	disk_conf_from_attrs(&dc, info);
+
+	printf("DEVICE=drbd%u\n", ((struct drbd_genlmsghdr*)(info->userhdr))->minor);
+	printf("SYMLINK_BY_RES=drbd/by-res/%s/%u\n", cfg.ctx_resource_name, cfg.ctx_volume);
+
+	if (!strncmp("/dev/", dc.backing_dev, 5))
+		printf("SYMLINK_BY_DISK=drbd/by-disk/%s\n", dc.backing_dev + 5);
+	else if (dc.backing_dev[0])
+		printf("SYMLINK_BY_DISK=drbd/by-disk/%s\n", dc.backing_dev);
+
 	return 0;
 }
 

user/v9/drbdsetup.c
@@ -179,6 +179,7 @@ static int cstate_cmd(struct drbd_cmd *cm, int argc, char **argv);
 static int dstate_cmd(struct drbd_cmd *cm, int argc, char **argv);
 static int check_resize_cmd(struct drbd_cmd *cm, int argc, char **argv);
 static int show_or_get_gi_cmd(struct drbd_cmd *cm, int argc, char **argv);
+static int udev_cmd(struct drbd_cmd *cm, int argc, char **argv);
 
 // sub commands for generic_get_cmd
        int print_event(struct drbd_cmd *, struct genl_info *, void *); /* is in drbdsetup_events2.c */
@@ -461,6 +462,9 @@ struct drbd_cmd commands[] = {
 		{ "new_name", T_new_resource_name, conv_str },
 		{ } },
 	.summary = "Rename a resource." },
+	{"udev", CTX_MINOR, 0, NO_PAYLOAD, udev_cmd,
+	.lockless = true,
+	.summary = "Generate output for udev rules."},
 };
 
 bool show_defaults;
@@ -3467,6 +3471,35 @@ static int dstate_cmd(struct drbd_cmd *cm, int argc, char **argv)
 				printf("/%s", drbd_disk_str(peer_device->info.peer_disk_state));
 		}
 		printf("\n");
+		found = true;
+		break;
+	}
+	free_devices(devices);
+
+	if (!found) {
+		fprintf(stderr, "%s: No such device\n", objname);
+		return 10;
+	}
+	return 0;
+}
+
+static int udev_cmd(struct drbd_cmd *cm, int argc, char **argv)
+{
+	struct devices_list *devices, *device;
+	bool found = false;
+
+	devices = list_devices(NULL);
+	for (device = devices; device; device = device->next) {
+		if (device->minor != minor)
+			continue;
+
+		printf("DEVICE=drbd%u\n", device->minor);
+		printf("SYMLINK_BY_RES=drbd/by-res/%s/%u\n", device->ctx.ctx_resource_name, device->ctx.ctx_volume);
+		if (!strncmp("/dev/", device->disk_conf.backing_dev, 5))
+			printf("SYMLINK_BY_DISK=drbd/by-disk/%s\n", device->disk_conf.backing_dev + 5);
+		else if (device->disk_conf.backing_dev[0])
+			printf("SYMLINK_BY_DISK=drbd/by-disk/%s\n", device->disk_conf.backing_dev);
+
 		found = true;
 		break;
 	}

commit 53847ad9bcd9ef48da9803825aa9a3690e1da79e

drbdsetup,84: fix udev command
The new drbdsetup udev command always returned a non-zero exit code. This was
caused by returning "1" when no "*genl_info" was available. However, this is
expected, as the command function is called once for the resource, and another
time afterwards with empty genl_info to indicate completion.

Fix this by returning 0 for the case the genl_info is null. All the validation
is already handled by the outer layers.

Fixes: 18f80de3d9ff ("drbdsetup: add udev command")

user/v84/drbdsetup.c
@@ -2320,7 +2320,7 @@ static int udev_scmd(const struct drbd_cmd *cm, struct genl_info *info, void *u_
 	struct disk_conf dc = { .disk_size = 0, };
 
 	if (!info) {
-		return 1;
+		return 0;
 	}
 
 	drbd_cfg_context_from_attrs(&cfg, info);