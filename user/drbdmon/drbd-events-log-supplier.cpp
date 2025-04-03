8b87c53dccb860ac9fc29bcc9d15aedf19b60280
Robert Altnoeder <robert.altnoeder@linbit.com>
2025-04-03 17:40:59 +0200

    DRBDmon: drbd-events-log-supplier timestamps compatibility
    Enables loading of event files into DRBDmon that were created
    using the --timestamps option with drbdsetup events2 by
    preprocessing the event lines before they are passed on to
    DRBDmon.

--- user/drbdmon/drbd-events-log-supplier.cpp
+++ user/drbdmon/drbd-events-log-supplier.cpp
@@ -11,11 +11,28 @@ extern "C"
 constexpr int ERR_OUT_OF_MEMORY     = 2;
 constexpr int ERR_IO                = 3;
 
+const std::string EVENT_EXISTS("exists");
+const std::string EVENT_CREATE("create");
+const std::string EVENT_CHANGE("change");
+const std::string EVENT_DESTROY("destroy");
+const std::string* const EVENT_TYPES[] =
+{
+    &EVENT_EXISTS,
+    &EVENT_CREATE,
+    &EVENT_CHANGE,
+    &EVENT_DESTROY,
+    nullptr
+};
 const std::string INIT_STATE_SEPA("exists -");
 const size_t      INIT_STATE_SEPA_LENGTH = INIT_STATE_SEPA.length();
 
 int process_events(const std::string& path);
 bool is_init_state_sepa(const std::string& event_line);
+static inline void process_event_line(
+    const bool skip_timestamp,
+    bool& have_init_state_sepa,
+    std::string& event_line
+);
 
 /**
  * DRBD events log file supplier for DRBDmon
@@ -49,6 +66,40 @@ int main(int argc, char* argv[])
     return exit_code;
 }
 
+static inline void process_event_line(
+    const bool skip_timestamp,
+    bool& have_init_state_sepa,
+    std::string& event_line
+)
+{
+    if (skip_timestamp)
+    {
+        size_t event_idx = 0;
+        size_t event_pos = std::string::npos;
+        while (event_pos == std::string::npos && EVENT_TYPES[event_idx] != nullptr)
+        {
+            const std::string* const event_str = EVENT_TYPES[event_idx];
+            event_pos = event_line.find(*event_str);
+            ++event_idx;
+        }
+        if (event_pos != std::string::npos)
+        {
+            event_line = event_line.substr(event_pos);
+        }
+    }
+
+    if (event_line.length() > 0)
+    {
+        std::cout << event_line << '\n';
+    }
+    if (is_init_state_sepa(event_line))
+    {
+        have_init_state_sepa = true;
+    }
+
+    event_line.clear();
+}
+
 /**
  * Emits event lines from an events log file and appends the initial state separator
  * if it is absent from the file. The output is stdout, which DRBDmon pipes to itself.
@@ -70,22 +121,24 @@ int process_events(const std::string& path)
         if (in_file.good())
         {
             bool have_init_state_sepa = false;
+            bool skip_timestamp = false;
+
+            std::string event_line;
+
+            // Determine presence of a timestamp before the event type by
+            // analyzing the first line
+            {
+                std::getline(in_file, event_line);
+                const size_t event_pos = event_line.find(EVENT_EXISTS);
+                skip_timestamp = event_pos != std::string::npos && event_pos != 0;
+                process_event_line(skip_timestamp, have_init_state_sepa, event_line);
+            }
 
             // Emit events from the file to DRBDmon
-            std::string event_line;
             while (in_file.good())
             {
                 std::getline(in_file, event_line);
-                if (event_line.length() > 0)
-                {
-                    std::cout << event_line << '\n';
-                }
-                if (is_init_state_sepa(event_line))
-                {
-                    have_init_state_sepa = true;
-                }
-
-                event_line.clear();
+                process_event_line(skip_timestamp, have_init_state_sepa, event_line);
             }
 
             if (in_file.eof())
