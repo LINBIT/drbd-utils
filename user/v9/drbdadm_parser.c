LINBIT/drbd-utils

commit 8b48c591bae3ec57625e81da55157aa70cec8844

drbdadm adjust: detect and drop options unsupported by kernel
drbdsetup show now detects options defined in "config_flags"
that are unsupported by the currently active kernel driver.

Such options are reported as "_unknown option-name".

drbdadm adjust now understands this new token, and silently drops
these options in case they are mentioned in the config file.

Otherwise "adjust" would try to re-apply some options
on every run, and be ignored by the kernel on every run.

The new option that triggered this change is "resync-without-replication".

user/v9/drbdadm_parser.c
@@ -661,6 +661,30 @@ static void __parse_options(struct options *options,
 		if (token == '}')
 			return;
 
+		if (token == TK__UNKNOWN) {
+			struct d_option *no;
+
+			token = yylex();
+
+			/* Our drbdsetup reported an option as unknown by the kernel.
+			 * If we don't know that option either,
+			 * how can drbdsetup know to report it as unknown?
+			 * That really should not happen.
+			 */
+			field_def = find_field(&no_prefix, options_def, yytext);
+			if (!field_def) {
+				char *s = yytext;
+				log_err("%s:%u: Parse error(ignored): '_unknown %.40s%s', but I don't know about it either.\n",
+				    config_file, line, s, strlen(s) > 40 ? "..." : "");
+				continue;
+			}
+
+			no = new_opt((char*)field_def->name, NULL);
+			no->unknown = true;
+			insert_tail(options, no);
+			continue;
+		}
+
 		field_def = find_field(&no_prefix, options_def, yytext);
 		if (!field_def) {
 			if (delegate) {

user/v9/drbdsetup.c
@@ -1270,6 +1270,34 @@ static struct drbd_cmd *find_cmd_by_name(const char *name)
 	return NULL;
 }
 
+bool should_show_field_info_from_attr_tb(struct nlattr **nlattr_p, const char **str_p, bool *is_default_p,
+		struct nlattr **nlattr_tb, struct field_def *field, struct context_def *ctx)
+{
+	/* If not present (*nlattr == NULL), kernel does not know about this
+	 * field, so can not support it.  Unless kernel chose to not report
+	 * specific fields, even though it knows about them. We currently do
+	 * not have those.  Pretend it was NOT set to the default,
+	 * so it is shown even without "--show-defaults". */
+
+	bool is_default = false;
+	struct nlattr *nlattr;
+	const char *str = NULL;
+
+	nlattr = nlattr_tb[__nla_type(field->nla_type)];
+	if (nlattr) {
+		str = field->ops->get(ctx, field, nlattr);
+		is_default = field->ops->is_default(field, str);
+	}
+	if (is_default_p)
+		*is_default_p = is_default;
+	if (nlattr_p)
+		*nlattr_p = nlattr;
+	if (str_p)
+		*str_p = str;
+
+	return !is_default || show_defaults;
+}
+
 static bool __print_options(struct nlattr *attr, struct context_def *ctx, const char *sect_name,
 			    bool opened, bool close)
 {
@@ -1290,31 +1318,33 @@ static bool __print_options(struct nlattr *attr, struct context_def *ctx, const 
 		const char *str;
 		bool is_default;
 
-		nlattr = nested_attr_tb[__nla_type(field->nla_type)];
-		if (!nlattr)
+		if (!should_show_field_info_from_attr_tb(
+					&nlattr, &str, &is_default,
+					nested_attr_tb, field, ctx))
 			continue;
-		str = field->ops->get(ctx, field, nlattr);
-		is_default = field->ops->is_default(field, str);
-		if (is_default && !show_defaults)
-			continue;
+
 		if (!opened) {
 			opened = true;
 			printI("%s {\n",sect_name);
 			++indent;
 		}
-		if (field->needs_double_quoting)
-			str = double_quote_string(str);
-		printI("%-16s\t%s;",field->name, str);
-		if (field->unit || is_default) {
-				printf(" # ");
-			if (field->unit)
-				printf("%s", field->unit);
-			if (field->unit && is_default)
-				printf(", ");
-			if (is_default)
-				printf("default");
+		if (nlattr) {
+			if (field->needs_double_quoting)
+				str = double_quote_string(str);
+			printI("%-16s\t%s;",field->name, str);
+			if (field->unit || is_default) {
+					printf(" # ");
+				if (field->unit)
+					printf("%s", field->unit);
+				if (field->unit && is_default)
+					printf(", ");
+				if (is_default)
+					printf("default");
+			}
+			printf("\n");
+		} else {
+			printI("_unknown %-14s\t# not supported by kernel\n", field->name);
 		}
-		printf("\n");
 	}
 	if (opened && close) {
 		--indent;
@@ -1333,22 +1363,20 @@ static int nr_printed_opts(struct context_def *ctx, struct field_def *start,
 		struct nlattr **nested_attr_tb)
 {
 	struct field_def *field;
-	const char *str;
-	bool is_default;
+	struct nlattr *nlattr;
 
 	int nr = -1;
 	if (start == NULL)
 		return nr;
 
 	for (field = start; field->name; field++) {
-		struct nlattr *nlattr = nested_attr_tb[__nla_type(field->nla_type)];
+		if (!should_show_field_info_from_attr_tb(&nlattr, NULL, NULL, nested_attr_tb, field, ctx))
+			continue;
+		/* Json is data only, no "comments".
+		 * If _we_ know about it, AND kernel knows about it, we'd report it.
+		 * If we know it, but we don't report it, it is unsuported by kernel. */
 		if (!nlattr)
 			continue;
-		str = field->ops->get(ctx, field, nlattr);
-		is_default = field->ops->is_default(field, str);
-		if (is_default && !show_defaults)
-			continue;
-
 		nr++;
 	}
 
@@ -1384,12 +1412,11 @@ static bool print_options_json(
 		const char *str;
 		bool is_default;
 
-		nlattr = nested_attr_tb[__nla_type(field->nla_type)];
-		if (!nlattr)
+		if (!should_show_field_info_from_attr_tb(
+					&nlattr, &str, &is_default,
+					nested_attr_tb, field, ctx))
 			continue;
-		str = field->ops->get(ctx, field, nlattr);
-		is_default = field->ops->is_default(field, str);
-		if (is_default && !show_defaults)
+		if (!nlattr) /* unsupported by kernel */
 			continue;
 		if (!opened) {
 			opened=1;
@@ -2106,21 +2133,9 @@ static bool options_empty(struct nlattr *attr, struct context_def *ctx)
 		fprintf(stderr, "nla_policy violation\n");
 	}
 
-	for (field = ctx->fields; field->name; field++) {
-		struct nlattr *nlattr;
-		const char *str;
-		bool is_default;
-
-		nlattr = nested_attr_tb[__nla_type(field->nla_type)];
-		if (!nlattr)
-			continue;
-		str = field->ops->get(ctx, field, nlattr);
-		is_default = field->ops->is_default(field, str);
-		if (is_default && !show_defaults)
-			continue;
-		return false;
-	}
-
+	for (field = ctx->fields; field->name; field++)
+		if (should_show_field_info_from_attr_tb(NULL, NULL, NULL, nested_attr_tb, field, ctx))
+			return false;
 	return true;
 }
 

user/v9/drbdadm.h
@@ -40,6 +40,7 @@ struct d_option
 	char* name;
 	char* value;
 	unsigned int mentioned  :1 ; // for the adjust command.
+	unsigned int unknown  :1 ; // for the adjust command.
 	unsigned int is_escaped :1 ;
 	unsigned int adj_skip :1;
 	unsigned int inherited :1;

user/v9/drbdadm_adjust.c
@@ -160,6 +160,11 @@ static int opts_equal(struct context_def *ctx, struct options *conf, struct opti
 	struct d_option *opt, *run_opt;
 
 	STAILQ_FOREACH(run_opt, run_base, link) {
+		if (run_opt->unknown) {
+			/* In case it was mentioned in config, pretend it was not. */
+			del_opt(conf, run_opt->name);
+			continue;
+		}
 		if (run_opt->adj_skip)
 			continue;
 

user/v9/drbdadm_parser.h
@@ -67,6 +67,7 @@ enum yytokentype {
 	TK__REMOTE_HOST,
 	TK__PEER_NODE_ID,
 	TK__IS_STANDALONE,
+	TK__UNKNOWN,
 	TK_PROXY,
 	TK_INSIDE,
 	TK_OUTSIDE,

user/v9/drbdadm_scanner.fl
@@ -105,6 +105,7 @@ _this_host 		{ DP; return TK__THIS_HOST;		}
 _remote_host 		{ DP; return TK__REMOTE_HOST;		}
 _peer_node_id 		{ DP; return TK__PEER_NODE_ID;		}
 _is_standalone		{ DP; return TK__IS_STANDALONE;		}
+_unknown		{ DP; return TK__UNKNOWN;		}
 template-file		{ DP; return TK_TEMPLATE_FILE;		}
 path			{ DP; return TK_PATH;			}
 sci			{ DP; CP; return TK_SCI;		}

commit c106f7be98a2a31ceb3f9274121fb1e0538e4195

drbdsetup,v9: End the _unknon statements with a semicolon(';')
For consistency with all other statements.

Fixes: 8b48c591 ("drbdadm adjust: detect and drop options unsupported by kernel")

user/v9/drbdadm_parser.c
@@ -688,12 +688,14 @@ static void __parse_options(struct options *options,
 				char *s = yytext;
 				log_err("%s:%u: Parse error(ignored): '_unknown %.40s%s', but I don't know about it either.\n",
 				    config_file, line, s, strlen(s) > 40 ? "..." : "");
+				EXP(';');
 				continue;
 			}
 
 			no = new_opt((char*)field_def->name, NULL);
 			no->unknown = true;
 			insert_tail(options, no);
+			EXP(';');
 			continue;
 		}
 

user/v9/drbdsetup.c
@@ -1367,7 +1367,7 @@ static bool __print_options(struct nlattr *attr, struct context_def *ctx, const 
 			}
 			printf("\n");
 		} else {
-			printI("_unknown %-14s\t# not supported by kernel\n", field->name);
+			printI("_unknown %-14s;\t# not supported by kernel\n", field->name);
 		}
 	}
 	if (opened && close) {