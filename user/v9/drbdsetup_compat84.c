LINBIT/drbd-utils

commit 787b16922ec37cc6b38723f3612884f86e2fc325

drbdsetup: 8.4 compat for attach options
The usual calling sequence of drbdsetup version 8.4 looks like that:

drbdsetup new-resource r84
drbdsetup new-minor r84 84 0
drbdmeta 84 v08 /dev/scratch/r84_0 internal apply-al
drbdsetup attach 84 /dev/scratch/r84_0 /dev/scratch/r84_0 internal \
	  --c-max-rate=333M --disk-flushes=no --fencing=resource-only
drbdsetup connect r84 ipv4:192.168.122.55:8000 ipv4:192.168.122.56:8000 \
	  --protocol=B

While drbd-9 expects:

drbdsetup new-resource r0 0
drbdsetup new-minor r0 10 0
drbdsetup new-peer r0 1 --_name=remote --fencing=resource-only --protocol=B
drbdsetup new-path r0 1 ipv4:192.168.122.55:7000 ipv4:192.168.122.56:7000
drbdsetup peer-device-options r0 2 0 --c-max-rate=333M
drbdmeta 10 v09 /dev/scratch/r0_0 internal repair-md
drbdmeta 10 v09 /dev/scratch/r0_0 internal apply-al
drbdsetup attach 10 /dev/scratch/r0_0 /dev/scratch/r0_0 internal \
	  --disk-flushes=no
drbdsetup connect r0 1

The drbd-8.4 version of drbdsetup attach used to have six additional
options: fencing, resync-rate, c-plan-ahead, c-delay-target,
c-max-rate, and c-min-rate. The fencing option moves to the
new-peer-command, the others are peer-device-options.

Move the options between the drbdsetup invocations by saving them into
temporary files in /var/run/drbd/compat-84. One challenge to overcome
is that only the new-minor command is aware of the relationship
between resources, minors, and volumes. So it already creates the
directory structure, and the following attach command can then rely on
the minor-%d symlink to guide it to the correct resource and volume.

user/v9/drbdsetup_compat84.c
@@ -25,8 +25,22 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #include "drbdsetup.h"
+#include "path.h"
+
+enum c84_ctx_key {
+	CCTX_MINOR,
+	CCTX_RES_VIA_MINOR,
+	CCTX_RESOURCE,
+	CCTX_PEER_DEVICE,
+};
+
+static bool load_opts(int *argc, char **argv, const struct drbd_cmd *cmd, enum c84_ctx_key key);
 
 /*
  * Compare two sockaddr_storage structures.
@@ -74,7 +88,7 @@ static void drbd8_compat_relevant_opts(const struct drbd_cmd *cm, char **argv_in
 	char *arg;
 	int c;
 
-	options = make_longoptions(cm);
+	options = make_longoptions(cm, false);
 	optind = 0;
 	for (;;) {
 		int idx;
@@ -83,27 +97,68 @@ static void drbd8_compat_relevant_opts(const struct drbd_cmd *cm, char **argv_in
 		if (c == -1)
 			break;
 		if (c >= 1000 || c == 0) {
-			arg = argv_in[optind];
+			arg = argv_in[optind - 1];
 			args_out[(*argc)++] = arg;
-			if (optarg)
-				args_out[(*argc)++] = optarg;
 		}
+		/* if (c == '?') continue; */
 	}
+}
+
+
+static void drbd8_compat_set_peer_device_options(void)
+{
+	const char *resname = global_ctx.ctx_resource_name;
+	char path[200], *pd_args[40];
+	struct dirent *dirent;
+	int i, vol, argc;
+	bool loaded;
+	DIR *dir;
+
+	context = CTX_PEER_DEVICE;
+	pd_args[0] = (char *)peer_device_options_cmd.cmd;
+
+	snprintf(path, 200, "%s/compat-84/res-%s", drbd_run_dir(), resname);
+	dir = opendir(path);
+	dirent = readdir(dir);
+	while (dirent) {
+		if (sscanf(dirent->d_name, "vol-%d", &vol) == 1) {
+			argc = 1;
+			global_ctx.ctx_volume = vol;
+			loaded = load_opts(&argc, pd_args, &peer_device_options_cmd,
+					   CCTX_PEER_DEVICE);
+			if (loaded) {
+				_generic_config_cmd(&peer_device_options_cmd, argc, pd_args);
+				for (i = 1; i < argc; i++)
+					free(pd_args[i]); /* load_opts() malloc()ed */
+			}
+		}
+
+		dirent = readdir(dir);
+	}
+	closedir(dir);
 }
 
 static int drbd8_compat_fake_new_peer(int peer_node_id, int argc_in, char **argv_in)
 {
 	char *new_peer_args[40];
-	int argc = 0;
+	int rv, i, argc_dyn_start, argc = 0;
+
+	context = CTX_PEER_NODE;
+	global_ctx.ctx_peer_node_id = peer_node_id;
 
 	new_peer_args[argc++] = (char *)new_peer_cmd.cmd;
 	new_peer_args[argc++] = "--_name=remote";
 
-	drbd8_compat_relevant_opts(&new_peer_cmd, argv_in, argc_in,
-				   new_peer_args, &argc);
-	context = CTX_PEER_NODE;
-	global_ctx.ctx_peer_node_id = peer_node_id;
-	return _generic_config_cmd(&new_peer_cmd, argc, new_peer_args);
+	drbd8_compat_relevant_opts(&new_peer_cmd, argv_in, argc_in, new_peer_args, &argc);
+	argc_dyn_start = argc;
+	load_opts(&argc, new_peer_args, &new_peer_cmd, CCTX_RESOURCE);
+
+	rv = _generic_config_cmd(&new_peer_cmd, argc, new_peer_args);
+	for (i = argc_dyn_start; i < argc; i++)
+		free(new_peer_args[i]); /* load_opts() malloc()ed them via getline() */
+
+	drbd8_compat_set_peer_device_options();
+	return rv;
 }
 
 static int drbd8_compat_fake_new_path(int peer_node_id, const char *my_addr,
@@ -302,4 +357,156 @@ int drbd8_compat_connect_or_disconnect(int argc, char **argv, const struct drbd_
 		return drbd8_compat_disconnect_special_case(argc, argv, cmd);
 	} else
 		return -1;
+}
+
+static void cctx_key_to_path(enum c84_ctx_key key, char *path, int p_len, const char *cmd_str)
+{
+	const char *resname = global_ctx.ctx_resource_name;
+	const char *dd = drbd_run_dir();
+	int vol = global_ctx.ctx_volume;
+
+
+	if (key == CCTX_MINOR) {
+		snprintf(path, p_len, "%s/compat-84/minor-%d/%s-opts", dd, minor, cmd_str);
+	} else if (key == CCTX_RES_VIA_MINOR) {
+		snprintf(path, p_len, "%s/compat-84/minor-%d/../%s-opts", dd, minor, cmd_str);
+	} else if (key == CCTX_RESOURCE) {
+		snprintf(path, p_len, "%s/compat-84/res-%s/%s-opts", dd, resname, cmd_str);
+	} else if (key == CCTX_PEER_DEVICE) {
+		snprintf(path, p_len,
+			 "%s/compat-84/res-%s/vol-%d/%s-opts", dd, resname, vol, cmd_str);
+	} else {
+		fprintf(stderr, "invalid value for cctx_key: %d\n", key);
+		exit(30);
+	}
+}
+
+static bool load_opts(int *argc, char **argv, const struct drbd_cmd *cmd, enum c84_ctx_key key)
+{
+	ssize_t nread;
+	size_t alloced_size;
+	char fname[200], *str;
+	FILE *f;
+
+	cctx_key_to_path(key, fname, 200, cmd->cmd);
+	f = fopen(fname, "r");
+	if (!f)
+		return false;
+	while (true) {
+		str = NULL;
+		alloced_size = 0;
+		nread = getline(&str, &alloced_size, f);
+		if (nread == -1)
+			break;
+		if (str[nread - 1] == '\n')
+			str[nread - 1] = 0;
+		argv[(*argc)++] = str;
+	}
+
+	fclose(f);
+	unlink(fname);
+	return true;
+}
+
+static void store_opts(int argc, char **argv, const struct drbd_cmd *cmd, enum c84_ctx_key key)
+{
+	char *opts[7], fname[100];
+	int i, nr_opts = 0;
+	FILE *f;
+
+	drbd8_compat_relevant_opts(cmd, argv, argc, opts, &nr_opts);
+	if (nr_opts == 0)
+		return;
+
+	cctx_key_to_path(key, fname, 100, cmd->cmd);
+	f = fopen(fname, "w");
+	if (!f) {
+		fprintf(stderr, "Failed to open '%s' for writing with %s (%d)\n",
+			fname, strerror(errno), errno);
+		exit(20);
+	}
+	for (i = 0; i < nr_opts; i++)
+		fprintf(f, "%s\n", opts[i]);
+	fclose(f);
+}
+
+/* drbd8_compat_attach()
+ * drbd-8.4 had six additional disk options
+ * fencing, c-plan-ahead, c-delay-target, c-fill-target, c-max-rate, c-min-rate
+ * fencing is moved to new-peer, the other five are moved to peer-device-options
+ */
+int drbd8_compat_attach(int argc_in, char **argv_in)
+{
+	static struct drbd_cmd attach_cmd_no_recursion;
+	static bool initialized = false;
+	char *attach_args[20];
+	int argc = 0;
+
+	if (!initialized) {
+		attach_cmd_no_recursion = attach_cmd;
+		initialized = true;
+	}
+
+	store_opts(argc_in, argv_in, &peer_device_options_cmd, CCTX_MINOR);
+	store_opts(argc_in, argv_in, &new_peer_cmd, CCTX_RES_VIA_MINOR);
+
+	attach_args[argc++] = (char *)attach_cmd.cmd;
+	attach_args[argc++] = (char *)argv_in[optind + 0]; /* lower_dev */
+	attach_args[argc++] = (char *)argv_in[optind + 1]; /* meta_data_dev */
+	attach_args[argc++] = (char *)argv_in[optind + 2]; /* meta_data_index */
+
+	drbd8_compat_relevant_opts(&attach_cmd, argv_in, argc_in, attach_args, &argc);
+
+	return _generic_config_cmd(&attach_cmd_no_recursion, argc, attach_args);
+}
+
+
+static void mkdir_or_exit(const char *path)
+{
+	struct stat sb;
+	int err;
+
+	err = stat(path, &sb);
+	if (!err) {
+		if (S_ISDIR(sb.st_mode))
+			return;
+		fprintf(stderr, "'%s' exists and is not a directory\n", path);
+		exit(20);
+	}
+	err = mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	if (err) {
+		fprintf(stderr, "Failed to mkdir '%s' with %s (%d)\n",
+			path, strerror(errno), errno);
+		exit(20);
+	}
+}
+
+void drbd8_compat_new_minor(const char *resname, const char *minor_str, const char *vol_str)
+{
+	char path[200], link[50], existing_link_target[50];
+	int minor = atoi(minor_str);
+	int vol = atoi(vol_str);
+	ssize_t r;
+	int err;
+
+	snprintf(path, 200, "%s/compat-84", drbd_run_dir());
+	mkdir_or_exit(path);
+
+	snprintf(path, 200, "%s/compat-84/res-%s", drbd_run_dir(), resname);
+	mkdir_or_exit(path);
+
+	snprintf(path, 200, "%s/compat-84/res-%s/vol-%d", drbd_run_dir(), resname, vol);
+	mkdir_or_exit(path);
+
+	snprintf(link, 50, "%s/compat-84/minor-%d", drbd_run_dir(), minor);
+	snprintf(path, 200, "res-%s/vol-%d", resname, vol);
+	r = readlink(link, existing_link_target, 50);
+	if (r > 0 && strncmp(path, existing_link_target, r) == 0)
+		return;
+	err = symlink(path, link);
+	if (err) {
+		fprintf(stderr, "Failed to create symlink '%s' with %s (%d)\n",
+			link, strerror(errno), errno);
+		exit(20);
+	}
 }

user/v9/config_flags.c
@@ -1368,3 +1368,16 @@ struct context_def startup_options_ctx = {
 struct context_def wildcard_ctx = {
        .fields = { { } },
 };
+
+#ifdef WITH_84_SUPPORT
+struct field_def attach_compat_84_fields[] = {
+	{ "fencing", ENUM(fencing_policy, FENCING) },
+	{ "resync-rate", NUMERIC(resync_rate, RESYNC_RATE), .unit = "bytes/second" },
+	{ "c-plan-ahead", NUMERIC(c_plan_ahead, C_PLAN_AHEAD), .unit = "1/10 seconds" },
+	{ "c-delay-target", NUMERIC(c_delay_target, C_DELAY_TARGET), .unit = "1/10 seconds" },
+	{ "c-fill-target", NUMERIC(c_fill_target, C_FILL_TARGET), .unit = "bytes" },
+	{ "c-max-rate", NUMERIC(c_max_rate, C_MAX_RATE), .unit = "bytes/second" },
+	{ "c-min-rate", NUMERIC(c_min_rate, C_MIN_RATE), .unit = "bytes/second" },
+	{ },
+};
+#endif

user/v9/config_flags.h
@@ -116,6 +116,9 @@ extern struct context_def proxy_options_ctx;
 extern struct context_def startup_options_ctx;
 extern struct context_def wildcard_ctx;
 
+#ifdef WITH_84_SUPPORT
+extern struct field_def attach_compat_84_fields[];
+#endif
 
 extern const char *double_quote_string(const char *str);
 

user/v9/drbdsetup.c
@@ -261,7 +261,11 @@ const struct drbd_cmd attach_cmd = {
 		{ "meta_data_index", T_meta_dev_idx, conv_md_idx },
 		{ } },
 	.ctx = &attach_cmd_ctx,
-	.summary = "Attach a lower-level device to an existing replicated device." };
+	.summary = "Attach a lower-level device to an existing replicated device.",
+#ifdef WITH_84_SUPPORT
+	.compat_84_fields = attach_compat_84_fields,
+#endif
+	};
 
 const struct drbd_cmd connect_cmd = {"connect", CTX_PEER_NODE,
 				     DRBD_ADM_CONNECT, DRBD_NLA_CONNECT_PARMS,
@@ -871,7 +875,7 @@ static int get_af_ssocks(int warn_and_use_default)
 	return af;
 }
 
-struct option *make_longoptions(const struct drbd_cmd *cm)
+struct option *make_longoptions(const struct drbd_cmd *cm, bool accept_84_compat)
 {
 	static struct option buffer[47];
 	int i = 0;
@@ -900,6 +904,22 @@ struct option *make_longoptions(const struct drbd_cmd *cm)
 		}
 		assert(field - cm->ctx->fields == i);
 	}
+
+#ifdef WITH_84_SUPPORT
+	if (accept_84_compat && cm->compat_84_fields) {
+		struct field_def *field;
+
+		for (field = cm->compat_84_fields; field->name; field++) {
+			assert(i < ARRAY_SIZE(buffer));
+			buffer[i].name = field->name;
+			buffer[i].has_arg = field->argument_is_optional ?
+				optional_argument : required_argument;
+			buffer[i].flag = NULL;
+			buffer[i].val = 0;
+			i++;
+		}
+	}
+#endif
 
 	if (cm->options) {
 		struct option *option;
@@ -1114,7 +1134,15 @@ int _generic_config_cmd(const struct drbd_cmd *cm, int argc, char **argv)
 
 	nla = NULL;
 
-	options = make_longoptions(cm);
+#ifdef WITH_84_SUPPORT
+	if (cm == &attach_cmd) {
+		msg_free(smsg);
+		free(iov.iov_base);
+		return drbd8_compat_attach(argc, argv);
+	}
+#endif
+
+	options = make_longoptions(cm, false);
 	optind = 0;  /* reset getopt_long() */
 	for (;;) {
 		int idx;
@@ -1148,6 +1176,11 @@ int _generic_config_cmd(const struct drbd_cmd *cm, int argc, char **argv)
 			goto error;
 		}
 	}
+
+#ifdef WITH_84_SUPPORT
+	if (cm == &new_minor_cmd)
+		drbd8_compat_new_minor(argv[optind + 0], argv[optind + 1], argv[optind + 2]);
+#endif
 
 	for (i = optind, ad = cm->drbd_args; ad && ad->name; i++) {
 		if (argc < i + 1) {
@@ -4726,7 +4759,8 @@ int drbdsetup_main(int argc, char **argv)
 			connect_options.rcvbuf_size = m_strtoll(val,1);
 	}
 
-	options = make_longoptions(cmd);
+	opterr = 0; /* omit messages on stderr by getopt_long() */
+	options = make_longoptions(cmd, true);
 	opts = make_optstring(options);
 	for (;;) {
 		c = getopt_long(argc, argv, opts, options, &longindex);

user/v9/drbdsetup.h
@@ -70,6 +70,9 @@ struct drbd_cmd {
 	bool lockless;
 	struct context_def *ctx;
 	const char *summary;
+#ifdef WITH_84_SUPPORT
+	struct field_def *compat_84_fields;
+#endif
 };
 
 enum {
@@ -147,6 +150,7 @@ extern bool opt_diff;
 extern bool opt_fullch;
 extern struct drbd_cfg_context global_ctx;
 extern enum cfg_ctx_key context;
+extern unsigned int minor;
 extern const struct drbd_cmd new_resource_cmd;
 extern const struct drbd_cmd new_minor_cmd;
 extern const struct drbd_cmd attach_cmd;
@@ -158,7 +162,7 @@ extern const struct drbd_cmd del_path_cmd;
 extern const struct drbd_cmd disconnect_cmd;
 extern const struct drbd_cmd peer_device_options_cmd;
 
-struct option *make_longoptions(const struct drbd_cmd *cm);
+struct option *make_longoptions(const struct drbd_cmd *cm, bool accept_84_compat);
 int _generic_config_cmd(const struct drbd_cmd *cm, int argc, char **argv);
 void print_command_usage(const struct drbd_cmd *cm, enum usage_type);
 int sockaddr_from_str(struct sockaddr_storage *storage, const char *str);

user/v9/drbdsetup_compat84.h
@@ -5,11 +5,18 @@
 
 #ifdef WITH_84_SUPPORT
 int drbd8_compat_connect_or_disconnect(int argc, char **argv, const struct drbd_cmd *cmd);
+int drbd8_compat_attach(int argc, char **argv);
+void drbd8_compat_new_minor(const char *resname, const char *minor_str, const char *vol_str);
 #else
 static inline int drbd8_compat_connect_or_disconnect(int argc, char **argv, const struct drbd_cmd *cmd)
 {
 	return 0;
 }
+static int drbd8_compat_attach(int argc, char **argv)
+{
+	return 0;
+}
+static void drbd8_compat_new_minor(const char *resname, const char *minor_str, const char* vol_str) {}
 #endif
 
 #endif /* __DRBD_SETUP_COMPAT84_H */

commit 6d41917f362afbd91f4d0d9704ca42fd73cb0cc1

drbdsetup: fix 8.4 compat when symlink already exists
When one creates a resource with a volume, downs the resource (or
reboots the machine), and then creates the same resource with the same
minor now being in a different volume, then the previous symlink was
in the way and drbdsetup failed with:

Failed to create symlink '/var/run/drbd/compat-84/minor-1006' with File exists (17)
Command 'drbdsetup new-minor crt-target-full 1006 0' terminated with exit code 20

Fix this by removing the existing previous symlink before creating the
new one.

Fixes: 787b16922ec3 ("drbdsetup: 8.4 compat for attach options")

user/v9/drbdsetup_compat84.c
@@ -477,8 +477,11 @@ void drbd8_compat_new_minor(const char *resname, const char *minor_str, const ch
 	snprintf(link, 50, "%s/compat-84/minor-%d", drbd_run_dir(), minor);
 	snprintf(path, 200, "res-%s/vol-%d", resname, vol);
 	r = readlink(link, existing_link_target, 50);
-	if (r > 0 && strncmp(path, existing_link_target, r) == 0)
-		return;
+	if (r > 0) {
+		if (strncmp(path, existing_link_target, r) == 0)
+			return;
+		unlink(link);
+	}
 	err = symlink(path, link);
 	if (err) {
 		fprintf(stderr, "Failed to create symlink '%s' with %s (%d)\n",