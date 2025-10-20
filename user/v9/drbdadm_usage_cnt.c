LINBIT/drbd-utils

commit 4ccbe44bec5ef3367c4d649f6647da9295199245

drbdadm: if config is v8 and driver has v8 compatibility behave as v8
When the presented configuration file contains only v8 statements and
the DRBD driver has v8 compatibility compiled in, execute the drbdadm
v8 command to create the resource in compatibility mode, which
presents a v8-style /proc/drbd entry.

user/v9/drbdadm_usage_cnt.c
@@ -61,24 +61,18 @@ struct node_info_od {
 } __packed;
 
 
-
-void maybe_exec_legacy_drbdadm(char **argv)
+void exec_legacy_drbdadm(char **argv)
 {
-	const struct version *driver_version = drbd_driver_version(FALLBACK_TO_UTILS);
-
-	if (driver_version->version.major == 8 &&
-	    driver_version->version.minor == 4) {
 #ifdef DRBD_LEGACY_84
-		/* This drbdadm warned already... */
-		setenv("DRBD_DONT_WARN_ON_VERSION_MISMATCH", "1", 0);
-		add_lib_drbd_to_path();
-		execvp(drbdadm_84, argv);
-		log_err("execvp() failed to exec %s: %m\n", drbdadm_84);
+	/* This drbdadm warned already... */
+	setenv("DRBD_DONT_WARN_ON_VERSION_MISMATCH", "1", 0);
+	add_lib_drbd_to_path();
+	execvp(drbdadm_84, argv);
+	log_err("execvp() failed to exec %s: %m\n", drbdadm_84);
 #else
-		config_help_legacy("drbdadm", driver_version);
+	config_help_legacy("drbdadm", driver_version);
 #endif
-		exit(E_EXEC_ERROR);
-	}
+	exit(E_EXEC_ERROR);
 }
 
 static char *vcs_to_str(struct version *rev)

user/v9/drbdadm.h
@@ -361,6 +361,11 @@ struct cfg_ctx {
 	const struct adm_cmd *cmd;
 };
 
+enum cfg_version {
+	CV_IS_V9,
+	CV_IS_V8,
+	CV_AMBIGOUS
+};
 
 extern char *canonify_path(const char *path);
 extern int pushd(const char *path);
@@ -417,7 +422,7 @@ void cancel_deferred_cmd(struct deferred_cmd *d);
 void cancel_deferred_waits(const struct d_resource *res);
 const struct adm_cmd *deferred_cmd(const struct deferred_cmd *dcmd);
 
-extern void maybe_exec_legacy_drbdadm(char **argv);
+extern void exec_legacy_drbdadm(char **argv);
 extern void uc_node(enum usage_count_type type);
 extern int have_ip(const char *af, const char *ip);
 extern void free_opt(struct d_option *item);
@@ -488,6 +493,7 @@ extern char* drbdadm_84;
 extern char ss_buffer[1024];
 extern const char *hostname;
 extern struct names backend_options;
+extern enum cfg_version config_version;
 
 /* ssprintf() places the result of the printf in the current stack
    frame and sets ptr to the resulting string. If the current stack

user/v9/drbdadm_main.c
@@ -220,6 +220,7 @@ int adjust_with_progress = 0;
 bool help;
 int do_verify_ips = 0;
 int do_register = 1;
+enum cfg_version config_version = CV_AMBIGOUS;
 /* if we want to adjust more than one resource,
  * instead of iteratively calling "drbdsetup show" for each of them,
  * call "drbdsetup show" once for all of them. */
@@ -3614,6 +3615,7 @@ void die_if_no_resources(void)
 
 int main(int argc, char **argv)
 {
+	const struct version *driver_version = drbd_driver_version(STRICT);
 	size_t i;
 	int rv = 0, r;
 	struct adm_cmd *cmd = NULL;
@@ -3653,7 +3655,9 @@ int main(int argc, char **argv)
 		exit(E_EXEC_ERROR);
 	}
 
-	maybe_exec_legacy_drbdadm(argv);
+	if (driver_version &&
+	    driver_version->version.major == 8 && driver_version->version.minor == 4)
+		exec_legacy_drbdadm(argv);
 
 	recognize_all_drbdsetup_options();
 	rv = parse_options(argc, argv, &cmd, &resource_names);
@@ -3712,6 +3716,9 @@ int main(int argc, char **argv)
 		exit(E_CONFIG_INVALID);
 
 	post_parse(&config, cmd->is_proxy_cmd ? MATCH_ON_PROXY : 0);
+
+	if (config_version == CV_IS_V8 && driver_version && driver_version->compat_84_present)
+		exec_legacy_drbdadm(argv);
 
 	if (!is_dump || dry_run || verbose)
 		expand_common();

user/v9/drbdadm_parser.c
@@ -1209,6 +1209,8 @@ static void parse_host_section(struct d_resource *res,
 				check_upr("node-id statement", "%s:%s:node-id", res->name, h->name);
 			check_upr("node-id", "%s:%s", res->name, host->node_id);
 			EXP(';');
+			if (config_version == CV_AMBIGOUS)
+				config_version = CV_IS_V9;
 			break;
 		case TK_OPTIONS:
 			EXP('{');
@@ -1303,6 +1305,8 @@ void parse_stacked_section(struct d_resource* res)
 				check_upr("node-id statement", "%s:%s:node-id", res->name, h->name);
 			check_upr("node-id", "%s:%s", res->name, host->node_id);
 			EXP(';');
+			if (config_version == CV_AMBIGOUS)
+				config_version = CV_IS_V9;
 			break;
 		case '}':
 			goto break_loop;
@@ -1883,12 +1887,18 @@ struct d_resource* parse_resource(char* res_name, enum pr_flags flags)
 			break;
 		case TK_CONNECTION:
 			insert_tail(&res->connections, parse_connection(flags));
+			if (config_version == CV_AMBIGOUS)
+				config_version = CV_IS_V9;
 			break;
 		case TK_CONNECTION_MESH:
 			parse_connection_mesh(res, flags);
+			if (config_version == CV_AMBIGOUS)
+				config_version = CV_IS_V9;
 			break;
 		case TK_TEMPLATE_FILE:
 			res->template = template_file(res_name);
+			if (config_version == CV_AMBIGOUS)
+				config_version = CV_IS_V9;
 			break;
 		case TK_SKIP:
 			parse_skip();
@@ -2184,6 +2194,8 @@ void my_parse(void)
 			EXP(TK_KMODVERS);
 			EXP(';');
 			validate_kmod(token);
+			if (config_version == CV_AMBIGOUS)
+				config_version = CV_IS_V9;
 			break;
 		case 0:
 			return;

user/v9/drbdadm_postparse.c
@@ -143,6 +143,9 @@ static struct d_host_info *find_host_info_by_address(struct d_resource* res, str
 /* This should be in sync with drbdsetup_compat84.c: compare_addr() */
 static bool generate_implicit_node_id(int *addr_hash, struct d_host_info **host_info_array)
 {
+	if (config_version == CV_AMBIGOUS)
+		config_version = CV_IS_V8;
+
 	if (addr_hash[0] > addr_hash[1]) {
 		host_info_array[0]->node_id = strdup("0");
 		host_info_array[1]->node_id = strdup("1");

user/v9/drbdtool_common.h
@@ -82,6 +82,7 @@ struct version {
 		unsigned major, minor, sublvl;
 	} version;
 	unsigned version_code;
+	bool compat_84_present;
 };
 
 /* Windows km/miniport.h has a #define STRICT */

user/v9/drbdtool_common_linux.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include "drbdtool_common.h"
 
 extern struct version __drbd_driver_version;
@@ -42,6 +43,8 @@ const struct version *get_drbd_driver_version(void)
 	version_txt = slurp_proc_drbd();
 	if (version_txt) {
 		parse_version(&__drbd_driver_version, version_txt);
+		if (strstr(version_txt, "(compat 8.4)"))
+			__drbd_driver_version.compat_84_present = true;
 		free(version_txt);
 		return &__drbd_driver_version;
 	} else {

commit a05df70fcdc8be23470f25e8e7eec8103b896990

drbdadm: fix compiling with configure --without-84support
Fix syntax error when DRBD_LEGACY_84 is not defined.

Fixes: 4ccbe44bec5ef ("drbdadm: if config is v8 and driver has v8 compatibility behave as v8")

user/v9/drbdadm_usage_cnt.c
@@ -70,6 +70,8 @@ void exec_legacy_drbdadm(char **argv)
 	execvp(drbdadm_84, argv);
 	log_err("execvp() failed to exec %s: %m\n", drbdadm_84);
 #else
+	const struct version *driver_version = drbd_driver_version(FALLBACK_TO_UTILS);
+
 	config_help_legacy("drbdadm", driver_version);
 #endif
 	exit(E_EXEC_ERROR);