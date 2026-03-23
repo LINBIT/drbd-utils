LINBIT/drbd-utils

commit 1203729e95f189410a0b4d0870bb08d91179eb68

drbdadm: create-md: new options --effective-size, --diskful-peers
Allow to pass through --effective-size and --diskful-peers to create-md.
If --effective-size is set, but --diskful-peers is not explicitly set,
caclulate the list of node-ids of the diskful peers,
and pass it down to drbdmeta.

user/v9/drbdadm_main.c
@@ -1473,6 +1473,9 @@ bool is_set_gi_single_node(const struct cfg_ctx *ctx)
 
 int _adm_drbdmeta(const struct cfg_ctx *ctx, int flags, char *argument)
 {
+	/* that's enough for "--diskful-peers=" + a comma separated list of 0 .. 31 and then some */
+	char diskful_peers[120] = "";
+
 	struct d_volume *vol = ctx->vol;
 	char *argv[MAX_ARGS];
 	int argc = 0;
@@ -1506,6 +1509,43 @@ int _adm_drbdmeta(const struct cfg_ctx *ctx, int flags, char *argument)
 	if (argument)
 		argv[NA(argc)] = argument;
 	add_setup_options(argv, &argc, ctx->cmd->drbdsetup_ctx);
+
+	/* For create-md, if effective-size is set,
+	 * add the list of "diskful" peer node ids for this volume.
+	 * Unless that is explicitly specified as well.  */
+	if (!strcmp("create-md", ctx->cmd->name)
+	&& find_backend_option("--effective-size")
+	&& !find_backend_option("--diskful-peers")) {
+		struct connection *conn;
+		char sep = '=';
+		char *pos = diskful_peers;
+		int len;
+		len = snprintf(pos, pos - diskful_peers + sizeof(diskful_peers), "--diskful-peers");
+		assert(len > 0 && pos + len < diskful_peers + sizeof(diskful_peers));
+		pos += len;
+		for_each_connection(conn, &ctx->res->connections) {
+			struct peer_device *peer_device;
+
+			if (conn->ignore)
+				continue;
+
+			STAILQ_FOREACH(peer_device, &conn->peer_devices, connection_link) {
+				if (peer_device->vnr == vol->vnr) {
+					if (!peer_diskless(peer_device)) {
+						len = snprintf(pos, pos - diskful_peers + sizeof(diskful_peers),
+								"%c%s", sep, conn->peer->node_id);
+						assert(len > 0 && pos + len < diskful_peers + sizeof(diskful_peers));
+						pos += len;
+						sep = ',';
+					}
+					break;
+				}
+			}
+		}
+		if (sep != '=')
+			argv[NA(argc)] = diskful_peers;
+	}
+
 	argv[NA(argc)] = 0;
 
 	return m_system_ex(argv, flags, ctx->res->name);

user/v9/config_flags.c
@@ -1226,6 +1226,8 @@ struct context_def create_md_ctx = {
 		{ .name = "al-stripes", .argument_is_optional = false },
 		{ .name = "al-stripe-size-kB", .argument_is_optional = false },
 		{ .name = "force", .argument_is_optional = true },
+		{ .name = "effective-size", .argument_is_optional = false },
+		{ .name = "diskful-peers", .argument_is_optional = false },
 		{ } },
 };
 

commit 0d52b652d8b0af7f2dc101cd5210315d18864feb

drbdadm: fix "potential" buffer overflow
Remaining size of buffer for snprintf is sizeof() - (pos - start_pointer).
Not (pos - start_pointer) + sizeof() :-/

Fixes: 1203729e drbdadm: create-md: new options --effective-size, --diskful-peers

The buffer was chosen large enough so it would never trigger:
we have at most 31 peers, the node ids are the minimal decimal string
representation of those numbers, there is a short prefix... will always be
shorter than the 120 bytes we have in the buffer.

But it is plain wrong.
And "smart" gcc + glibc still detect it as buffer overflow,
___snprintf_chk (s=s@entry=0x7fffffffde2f "",
	maxlen=maxlen@entry=135, flag=flag@entry=2,
	    slen=slen@entry=105 ...
*** buffer overflow detected ***: terminated
Aborted (core dumped)

user/v9/drbdadm_main.c
@@ -1639,7 +1639,7 @@ int _adm_drbdmeta(const struct cfg_ctx *ctx, int flags, char *argument)
 		char sep = '=';
 		char *pos = diskful_peers;
 		int len;
-		len = snprintf(pos, pos - diskful_peers + sizeof(diskful_peers), "--diskful-peers");
+		len = snprintf(pos, sizeof(diskful_peers) - (pos - diskful_peers), "--diskful-peers");
 		assert(len > 0 && pos + len < diskful_peers + sizeof(diskful_peers));
 		pos += len;
 		for_each_connection(conn, &ctx->res->connections) {
@@ -1651,7 +1651,7 @@ int _adm_drbdmeta(const struct cfg_ctx *ctx, int flags, char *argument)
 			STAILQ_FOREACH(peer_device, &conn->peer_devices, connection_link) {
 				if (peer_device->vnr == vol->vnr) {
 					if (!peer_diskless(peer_device)) {
-						len = snprintf(pos, pos - diskful_peers + sizeof(diskful_peers),
+						len = snprintf(pos, sizeof(diskful_peers) - (pos - diskful_peers),
 								"%c%s", sep, conn->peer->node_id);
 						assert(len > 0 && pos + len < diskful_peers + sizeof(diskful_peers));
 						pos += len;