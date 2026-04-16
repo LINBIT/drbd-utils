LINBIT/drbd-utils

commit 999b97b7838f2c13715a4af0add35db1c72885e4

drbdsetup: allow to pass options to our genl_connect()
Pass options to genl_connect(). For now, that is send- and receive buffer sizes.
"Too much" activity causes many events to be multicast via netlink.
That may overrun the receive buffer.
We want to be able to increase the receive buffer size in large or busy setups.

user/shared/libgenl.c
@@ -36,11 +36,22 @@ int genl_join_mc_group(struct genl_sock *s, const char *name) {
 		}						\
 	} while(0)
 
-static struct genl_sock *genl_connect(__u32 nl_groups)
+static struct genl_sock *genl_connect(__u32 nl_groups, struct genl_connect_options *opts)
 {
+	struct genl_connect_options default_options = {
+		.rcvbuf_size = 1 << 20,
+		.sndbuf_size = 1 << 20,
+	};
 	struct genl_sock *s = calloc(1, sizeof(*s));
 	socklen_t sock_len;
-	int bsz = 1 << 20;
+	int bsz;
+
+	if (!opts)
+		opts = &default_options;
+	else {
+		if (!opts->rcvbuf_size) opts->rcvbuf_size = default_options.rcvbuf_size;
+		if (!opts->sndbuf_size) opts->sndbuf_size = default_options.sndbuf_size;
+	}
 
 	if (!s)
 		return NULL;
@@ -64,10 +75,24 @@ static struct genl_sock *genl_connect(__u32 nl_groups)
 		goto fail;
 
 	sock_len = sizeof(s->s_local);
-	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_SNDBUF, &bsz, sizeof(bsz)));
-	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUF, &bsz, sizeof(bsz)));
+	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_SNDBUF, &opts->sndbuf_size, sizeof(&opts->sndbuf_size)));
+	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUF, &opts->rcvbuf_size, sizeof(&opts->rcvbuf_size)));
 	DO_OR_LOG_AND_FAIL(bind(s->s_fd, (struct sockaddr*) &s->s_local, sizeof(s->s_local)));
 	DO_OR_LOG_AND_FAIL(getsockname(s->s_fd, (struct sockaddr*) &s->s_local, &sock_len));
+
+	/* Linux doubles the value set by setsockopt.
+	 * Compare half of what getsockopt reports back. */
+	sock_len = sizeof(bsz);
+	if (getsockopt(s->s_fd, SOL_SOCKET, SO_SNDBUF, &bsz, &sock_len) == 0
+	&&  (bsz/2) < opts->sndbuf_size)
+		dbg(1, "tried to set SO_SNDBUF %d, got %d; you may need to adjust sysctl net.core.wmem_max\n",
+			opts->sndbuf_size, (bsz/2));
+	sock_len = sizeof(bsz);
+	if (getsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUF, &bsz, &sock_len) == 0
+	&&  (bsz/2) < opts->rcvbuf_size) {
+		dbg(1, "tried to set SO_RCVBUF %d, got %d; you may need to adjust sysctl net.core.rmem_max\n",
+			opts->rcvbuf_size, (bsz/2));
+	}
 
 	dbg(3, "bound socket to nl_pid:%u, my pid:%u, len:%u, sizeof:%u\n",
 		s->s_local.nl_pid, getpid(),
@@ -275,7 +300,7 @@ static struct genl_family genl_ctrl = {
         .maxattr = CTRL_ATTR_MAX,
 };
 
-struct genl_sock *genl_connect_to_family(struct genl_family *family)
+struct genl_sock *genl_connect_to_family(struct genl_family *family, struct genl_connect_options *opts)
 {
 	struct genl_sock *s = NULL;
 	struct msg_buff *msg;
@@ -293,7 +318,7 @@ struct genl_sock *genl_connect_to_family(struct genl_family *family)
 		goto out;
 	}
 
-	s = genl_connect(family->nl_groups);
+	s = genl_connect(family->nl_groups, opts);
 	if (!s) {
 		dbg(1, "error creating netlink socket");
 		goto out;

user/shared/libgenl.h
@@ -1071,7 +1071,11 @@ struct genl_sock {
 	struct genl_family	*s_family;
 };
 
-extern struct genl_sock *genl_connect_to_family(struct genl_family *family);
+struct genl_connect_options {
+	int rcvbuf_size;
+	int sndbuf_size;
+};
+extern struct genl_sock *genl_connect_to_family(struct genl_family *family, struct genl_connect_options *opts);
 extern int genl_join_mc_group(struct genl_sock *s, const char *name);
 extern int genl_send(struct genl_sock *s, struct msg_buff *msg);
 enum {

user/shared/libgenl_windrbd.c
@@ -204,7 +204,7 @@ int genl_recv_msgs(struct genl_sock *s, struct iovec *iov, char **err_desc, int 
 	return verify_header(s, iov, size, err_desc);
 }
 
-struct genl_sock *genl_connect_to_family(struct genl_family *family)
+struct genl_sock *genl_connect_to_family(struct genl_family *family, struct genl_connect_options *opts)
 {
 	struct genl_sock *s = NULL;
 

user/v84/drbdsetup.c
@@ -3611,9 +3611,14 @@ int main(int argc, char **argv)
 	}
 
 	if (try_genl) {
+		struct genl_connect_options connect_options = {
+			.rcvbuf_size = 1024*1024,
+			.sndbuf_size =  128*1024,
+		};
+
 		if (cmd->continuous_poll && kernel_older_than(2, 6, 23))
 			drbd_genl_family.nl_groups = -1;
-		drbd_sock = genl_connect_to_family(&drbd_genl_family);
+		drbd_sock = genl_connect_to_family(&drbd_genl_family, &connect_options);
 		if (!drbd_sock) {
 			fprintf(stderr, "Could not connect to 'drbd' generic netlink family\n");
 			return 20;

user/v9/drbdsetup.c
@@ -4614,6 +4614,10 @@ static void maybe_exec_legacy_drbdsetup(char **argv)
 
 int drbdsetup_main(int argc, char **argv)
 {
+	struct genl_connect_options connect_options = {
+		.rcvbuf_size = 1024*1024,
+		.sndbuf_size =  128*1024,
+	};
 	const struct drbd_cmd *cmd;
 	struct option *options;
 	const char *opts;
@@ -4710,7 +4714,7 @@ int drbdsetup_main(int argc, char **argv)
 		/* maybe more specific: (1 << GENL_ID_CTRL)? */
 		drbd_genl_family.nl_groups = -1;
 	}
-	drbd_sock = genl_connect_to_family(&drbd_genl_family);
+	drbd_sock = genl_connect_to_family(&drbd_genl_family, &connect_options);
 	if (!drbd_sock) {
 		fprintf(stderr, "Could not connect to 'drbd' generic netlink family\n");
 		return 20;

commit a88775dfc555dd0d5afabac3421f33fac3900261

libgenl: fix incorrect sizeof in setsockopt calls
sizeof(&opts->sndbuf_size) gives the size of a pointer (8 bytes on
64-bit) instead of the size of the int (4 bytes). The original code
correctly used sizeof(bsz) with a local int variable; the typo was
introduced when refactoring to use the opts struct members.

Fixes: 999b97b7838f ("drbdsetup: allow to pass options to our genl_connect()")

user/shared/libgenl.c
@@ -75,8 +75,8 @@ static struct genl_sock *genl_connect(__u32 nl_groups, struct genl_connect_optio
 		goto fail;
 
 	sock_len = sizeof(s->s_local);
-	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_SNDBUF, &opts->sndbuf_size, sizeof(&opts->sndbuf_size)));
-	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUF, &opts->rcvbuf_size, sizeof(&opts->rcvbuf_size)));
+	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_SNDBUF, &opts->sndbuf_size, sizeof(opts->sndbuf_size)));
+	DO_OR_LOG_AND_FAIL(setsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUF, &opts->rcvbuf_size, sizeof(opts->rcvbuf_size)));
 	DO_OR_LOG_AND_FAIL(bind(s->s_fd, (struct sockaddr*) &s->s_local, sizeof(s->s_local)));
 	DO_OR_LOG_AND_FAIL(getsockname(s->s_fd, (struct sockaddr*) &s->s_local, &sock_len));
 
@@ -92,7 +92,7 @@ static struct genl_sock *genl_connect(__u32 nl_groups, struct genl_connect_optio
 	if (getsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUF, &bsz, &sock_len) == 0) {
 		if ((bsz/2) < opts->rcvbuf_size) {
 			/* retry with FORCE */
-			setsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUFFORCE, &opts->rcvbuf_size, sizeof(&opts->rcvbuf_size));
+			setsockopt(s->s_fd, SOL_SOCKET, SO_RCVBUFFORCE, &opts->rcvbuf_size, sizeof(opts->rcvbuf_size));
 		}
 	}
 #endif